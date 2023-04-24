dd8fd28b17be5a8519ea3cfb721969e149afbd91
andrewgreen76 <111017043+andrewgreen76@users.noreply.github.com>
2023-04-23 21:26:45 -0400

    Add files via upload

---
+++ PROG/((SANDBOX/sample.asm
@@ -0,0 +1,15 @@
+org 100h       
+mov ax, 13h 
+int 10h       
+mov ax, 40960
+mov ds, ax
+mov ax, 2Fh         
+mov bx, 0 ; will hold addr of pxl to paint in.   
+pntLnClr: 
+mov [bx],ax ; paint color in ax into pixel at bx. 
+inc bx
+cmp bx, 320
+JL pntLnClr
+mov ah, 0 
+int 16h 
+ret

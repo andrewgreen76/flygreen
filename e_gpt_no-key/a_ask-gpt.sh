f7ffed41c80210c79b30a136b10aa2266f1be2f4
Andrew Green <andrewgreen031991@gmail.com>
2023-04-26 11:32:22 -0400

    x

--- e_gpt_no-key/a_ask-gpt.sh
+++ e_gpt_no-key/a_ask-gpt.sh
@@ -1,7 +1,18 @@
 emacs seg_d_req.curl
 echo -e "\n" >> transcript.txt
 echo -e "\n" >> transcript.txt
-cat seg_d_req.curl
-cat seg_d_req.curl >> transcript.txt
+cat seg_d_req.curl                   # reflects query
+cat seg_d_req.curl >> transcript.txt # moves query to transcript
 echo -e "\n" >> transcript.txt
 echo -e "\n" >> transcript.txt
+cat seg_a_head.curl >> req_in_ba.sh
+cat seg_b_head.curl >> req_in_ba.sh
+cat seg_c_head.curl >> req_in_ba.sh
+cat seg_d_head.curl >> req_in_ba.sh
+cat seg_e_head.curl >> req_in_ba.sh # POST request assembled 
+bash req_in_ba.sh >> transcript.txt
+cd ..
+git add *
+git commit -m "x"
+git push
+cat transcript.txt

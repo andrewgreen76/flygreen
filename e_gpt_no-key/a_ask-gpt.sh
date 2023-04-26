cde6cea6ab9c84bbd7e608813e1cbd212871b13b
Andrew Green <andrewgreen031991@gmail.com>
2023-04-26 11:40:40 -0400

    x

--- e_gpt_no-key/a_ask-gpt.sh
+++ e_gpt_no-key/a_ask-gpt.sh
@@ -6,10 +6,10 @@ cat seg_d_req.curl >> transcript.txt # moves query to transcript
 echo -e "\n" >> transcript.txt
 echo -e "\n" >> transcript.txt
 cat seg_a_head.curl >> req_in_ba.sh
-cat seg_b_head.curl >> req_in_ba.sh
-cat seg_c_head.curl >> req_in_ba.sh
-cat seg_d_head.curl >> req_in_ba.sh
-cat seg_e_head.curl >> req_in_ba.sh # POST request assembled 
+cat seg_b_key.curl >> req_in_ba.sh
+cat seg_c_mid.curl >> req_in_ba.sh
+cat seg_d_req.curl >> req_in_ba.sh
+cat seg_e_tail.curl >> req_in_ba.sh # POST request assembled 
 bash req_in_ba.sh >> transcript.txt
 cd ..
 git add *

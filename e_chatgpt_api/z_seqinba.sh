61887c4726b0746a87dc837f9bf1e0d54ebb81cd
Andrew Green <andrewgreen031991@gmail.com>
2023-04-26 08:07:12 -0400

    x

---
+++ e_chatgpt_api/z_seqinba.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $YOUR-OPEN-API-KEY-HERE-WITHOUT-$" -d '{"prompt": "Hello, ChatGPT!", "temperature": 0.5, "max_tokens": 50}' https://api.openai.com/v1/completions

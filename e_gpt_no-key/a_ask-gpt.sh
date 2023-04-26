93ac2cd1b66e8759195314f9bbbc950abbf539ed
Andrew Green <andrewgreen031991@gmail.com>
2023-04-26 10:24:56 -0400

    x

--- e_gpt_no-key/a_ask-gpt.sh
+++ e_gpt_no-key/a_ask-gpt.sh
@@ -1,6 +1,7 @@
 emacs b_reqin.txt
 echo -e "\n" >> transcript.txt
 echo -e "\n" >> transcript.txt
+cat b_reqin.txt
 cat b_reqin.txt >> transcript.txt
 echo -e "\n" >> transcript.txt
 echo -e "\n" >> transcript.txt

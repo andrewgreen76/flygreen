aea93e4ffec1fa1088c2914bbe81c0f8131fc490
Andrew Green <andrewgreen031991@gmail.com>
2023-05-22 23:48:41 -0400

    x

---
+++ e_hkbk/d_on-top_adv/5_py_venv_ai-adv/tensorflow_code.py
@@ -0,0 +1,10 @@
+
+import tensorflow as tf
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+#===========================================================================
+
+# python -c "import tensorflow as tf; print('Num GPUs Available: ', len(tf.config.experimental.list_physical_devices('GPU')))"
+
+#===========================================================================
+

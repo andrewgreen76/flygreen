6f4fb41dab84e93479baa64d673be17dcc032493
Andrew Green <andrewgreen031991@gmail.com>
2023-05-23 06:40:52 -0400

    x

---
+++ e_hkbk/b_how-one-can-gift-magic/k_net_dsgns_code/f_06_speed_test/a.sh
@@ -0,0 +1,7 @@
+echo | tee -a b_reports.txt
+date | tee -a b_reports.txt
+echo | tee -a b_reports.txt
+speedtest-cli | tee -a b_reports.txt
+echo | tee -a b_reports.txt
+echo | tee -a b_reports.txt
+echo | tee -a b_reports.txt

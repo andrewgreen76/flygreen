bd165cd40ab9732ba3bc30d577bfbd9c8e7c95a2
Andrew Green <andrewgreen031991@gmail.com>
2023-05-24 19:48:14 -0400

    x

--- e_hkbk/b_how-one-can-gift-magic/n_py_ai-recogn_auto/boring_algs/find.py
+++ e_hkbk/b_how-one-can-gift-magic/n_py_ai-recogn_auto/boring_algs/find.py
@@ -3,3 +3,9 @@ if text.find("C++"):
     print("Found C++!")
 else:
     print("C++ not found!")
+
+# Yet text.find() returned -1. and text.find() is a Python function. text.find() would return index=-1 meaning "not found". Should have been: 
+#
+# if(text.find(__)==-1) print("not found")
+# else:
+#    print ("found")

4fa71bff031457f0a0530a10fea3693bd758e436
Andrew Green <andrewgreen031991@gmail.com>
2023-05-26 15:07:12 -0400

    x

--- e_hkbk/b_gifting-magic/3_linux/e_symlinks/shexe-sym-u.sh
+++ e_hkbk/b_gifting-magic/3_linux/e_symlinks/shexe-sym-u.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Specify the source file or directory
-source_file="~/flygreen/u_web/."
+source_file="$HOME/flygreen/u_web/."
 
 # Specify the target location and name of the symlink
 symlink_name="to-u"

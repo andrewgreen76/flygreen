3433a1e20d6abde579df87cd8fb1a9c2d62e118c
Andrew Green <andrewgreen031991@gmail.com>
2023-05-26 15:42:41 -0400

    x

--- e_hkbk/b_gifting-magic/3_linux/e_symlinks/shexe-sym-u.sh
+++ e_hkbk/b_gifting-magic/3_linux/e_symlinks/shexe-sym-u.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Specify the source file or directory
-source_file="$HOME/flygreen/u_web"
+source_file="/home/andrew/flygreen/u_web"
 
 # Specify the target location and name of the symlink
-symlink_name="to-u"
+symlink_name="tou"
 
 # Create the symbolic link
 ln -s "$source_file" "$symlink_name"

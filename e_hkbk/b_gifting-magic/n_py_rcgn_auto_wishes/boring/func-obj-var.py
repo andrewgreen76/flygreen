f11d718f4425180dd876451da2b8433ae3d30315
Andrew Green <andrewgreen031991@gmail.com>
2023-05-27 14:23:47 -0400

    x

---
+++ e_hkbk/b_gifting-magic/n_py_rcgn_auto_wishes/boring/func-obj-var.py
@@ -0,0 +1,15 @@
+class Run:
+    def __init__(self):
+        self.__miles = 7
+        self.races = 5
+
+        def run(self):
+            if self.__miles == 7:
+                return self.__miles
+            self.races = 6
+            return self.__miles * self.races
+
+
+t = Run()
+t._Run__miles = 5
+print(t.run())

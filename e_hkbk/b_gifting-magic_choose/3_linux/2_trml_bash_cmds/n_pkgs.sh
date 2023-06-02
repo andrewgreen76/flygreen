258b587a9518907fcdccad20edee17ecd01c77d1
Andrew Green <andrewgreen031991@gmail.com>
2023-06-02 17:07:01 -0400

    x

---
+++ e_hkbk/b_gifting-magic_choose/3_linux/2_trml_bash_cmds/n_pkgs.sh
@@ -0,0 +1,28 @@
+
+#==================== PACKAGES: essential Linux packages: ======================
+
+# Text editors: Some Linux systems come with nano by default, which is nice. 
+# But if you are curious, you are welcome to explore these: 
+sudo apt-get install emacs
+sudo apt-get install vim
+# Emacs prides itself on its ability to "intuit" approximately what the user wants to do. 
+
+sudo apt-get install gcc    # for C programs 
+sudo apt-get install g++    # for C++ programs
+
+# Python
+sudo apt-get install python3
+sudo apt-get install python3-pip
+# For more involved Python projects that must be isolated
+# to avoid package version conflicts: 
+sudo apt-get install python3-pyqt6
+sudo apt-get install python3.10-venv
+
+# for Java programs: 
+sudo apt-get install default-jre
+sudo apt-get install default-jdk
+
+# Whenever I get the time and the energy to remember and explain what these are for: 
+sudo apt-get install pkg-config
+sudo apt-get install libgtk-3-dev # installed by default 
+

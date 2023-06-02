55b1281d84d4402730e49aef29696e858820bf24
Andrew Green <andrewgreen031991@gmail.com>
2023-06-02 16:56:24 -0400

    x

---
+++ e_hkbk/b_gifting-magic_choose/3_linux/2_trml_bash_cmds/m_pkgs.sh
@@ -0,0 +1,17 @@
+
+==================== PACKAGES: essential Linux packages: ======================
+
+sudo apt-get install vim
+sudo apt-get install gcc
+sudo apt-get install g++
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo apt-get install python3-pyqt6
+sudo apt-get install python3.10-venv
+sudo apt-get install default-jre
+sudo apt-get install default-jdk
+sudo apt-get install pkg-config
+sudo apt-get install libgtk-3-dev //installed by default 
+
+
+

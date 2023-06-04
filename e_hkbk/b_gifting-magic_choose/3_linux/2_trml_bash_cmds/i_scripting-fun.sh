7d7b269a5edf016c9481668cc6d171b80f57353c
Andrew Green <andrewgreen031991@gmail.com>
2023-06-03 22:24:23 -0400

    x

--- e_hkbk/b_gifting-magic_choose/3_linux/2_trml_bash_cmds/i_scripting-fun.sh
+++ e_hkbk/b_gifting-magic_choose/3_linux/2_trml_bash_cmds/i_scripting-fun.sh
@@ -1,7 +1,7 @@
 echo 
-echo Hi. I\'m not home right now, but if you want to leave a message, just start talking at the sound of the tone. 
+echo \> Hi. I\'m not home right now, but if you want to leave a message, just start talking at the sound of the tone. 
 echo 
-echo Hello?
-echo This is your mother. Are you there?
-echo Are you coming home?
+echo \> Hello?
+echo \> This is your mother. Are you there?
+echo \> Are you coming home?
 echo 

a95f1e10120ceb0b439c3bb96f0092a71d4b0955
Andrew Green <andrewgreen031991@gmail.com>
2023-06-09 18:48:11 -0400

    x

---
+++ e_hkbk/a_win_games_trix+/f_py_cool-things/3_how-to-run.sh
@@ -0,0 +1,6 @@
+##!/bin/bash
+
+# python3 [script].py	- runs a Python script (Python 3 - latest version series)
+python3 2_hello.py
+# pip show		# shows info about a Python package(s) 
+
--- e_hkbk/a_win_games_trix+/f_py_cool-things/3_start.sh
+++
@@ -1,4 +0,0 @@
-
-python3 [script].py	- runs a Python script (Python 3 - latest version series)
-# pip show		# shows info about a Python package(s) 
-

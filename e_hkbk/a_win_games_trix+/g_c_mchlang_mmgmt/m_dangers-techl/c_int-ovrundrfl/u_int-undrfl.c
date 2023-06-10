4755137b1aa3d6a1705fc7c14f73720faf606e5b
Andrew Green <andrewgreen031991@gmail.com>
2023-06-10 08:24:16 -0400

    x

---
+++ e_hkbk/a_win_games_trix+/g_c_mchlang_mmgmt/m_dangers-techl/c_int-ovrundrfl/u_int-undrfl.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int a = INT_MIN;  // Minimum value for an 'int'
+    int b = 1;
+    int result = a - b;  // Integer underflow occurs here
+
+    printf("Result: %d\n", result);
+    return 0;
+}

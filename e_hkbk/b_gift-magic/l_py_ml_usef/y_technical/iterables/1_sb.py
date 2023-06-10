212291eaf0f811c295ef42b303247b2c867d2ae0
Andrew Green <andrewgreen031991@gmail.com>
2023-06-10 12:31:10 -0400

    x

--- e_hkbk/b_gift-magic/l_py_ml_usef/y_technical/iterables/1_sb.py
+++ e_hkbk/b_gift-magic/l_py_ml_usef/y_technical/iterables/1_sb.py
@@ -8,5 +8,6 @@ iter1, iter2 = itertools.tee(lst, 2) # not enough / too many vals to unpk.
 
 #print("Iter 1 - list: ", list(iter1))
 print("Itertr 1 - next: ", next(iter1))
+print("Itertr 1 - next: ", next(iter1))
 print("Itertr 1 - list: ", list(iter1))
 print("Itertr 2 - list: ", list(iter2))

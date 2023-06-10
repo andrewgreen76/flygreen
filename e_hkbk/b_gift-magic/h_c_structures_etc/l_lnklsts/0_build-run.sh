a60955eeb33a4136247f17dec3f46e9be5b6ffa4
Andrew Green <andrewgreen031991@gmail.com>
2023-06-10 18:36:50 -0400

    x

---
+++ e_hkbk/b_gift-magic/h_c_structures_etc/l_lnklsts/0_build-run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gcc b_bad-taste.c -o run.out
+
+./run.out
+
---
+++ e_hkbk/b_gift-magic/h_c_structures_etc/l_lnklsts/b_bad-taste.c
@@ -0,0 +1,39 @@
+#include "stdio.h"
+
+struct Node{
+  struct Node * next;
+};
+
+int main()
+{
+  // Making sure that it runs: 
+  printf("\nHello, world!\n\n");
+
+  // Build the linked list: 
+  struct Node t;
+  t.next = NULL;
+  struct Node s;
+  s.next = &t;
+  struct Node f;
+  f.next = &s;
+  struct Node * head = &f;
+
+  // Method 1: 
+  struct Node * prev = NULL;
+  struct Node * walk = *head;
+
+  while(*walk != &s){
+    prev = *walk;
+    walk = *(walk.next);
+  }
+
+  if(!prev)  // if prev was not assigned, meaning: if the 1st node is the target node "s". 
+    head = *(s.next);
+  else   // if "s" is not the 1st node: 
+    prev.next = *(s.next);
+
+  printf("\nDone.\n\n");
+  
+  //   
+  return 0; 
+}

712db9ca147b0c80cde19a6e77a46a82e0240f0a
Andrew Green <andrewgreen031991@gmail.com>
2023-06-10 08:13:35 -0400

    x

--- e_hkbk/a_win_games_trix+/g_c_mchlang_mmgmt/3_basic-progs/i_macros/9_int-min-max.c
+++ e_hkbk/a_win_games_trix+/g_c_mchlang_mmgmt/3_basic-progs/i_macros/9_int-min-max.c
@@ -1,10 +1,16 @@
 #include "stdio.h"
-#include "limits.h"
+#include "limits.h" // for ints
+#include "float.h" // for doubles
 
 int main()
 {
   printf("\nINT_MIN is: %d\n", INT_MIN);
-  printf("INT_MAX is: %d\n\n", INT_MAX);
+  printf("INT_MAX is: %d\n", INT_MAX);
+  printf("size of int is: %ld\n\n", sizeof(int));
+
+  printf("\nDBL_MIN is: %f\n", DBL_MIN);
+  printf("DBL_MAX is: %f\n", DBL_MAX);
+  printf("size of double is: %ld\n\n", sizeof(double));
   
   return 0;
 }

de648cdd7c87fe023e7751e1a903dfbfd876e101
Andrew Green <andrewgreen031991@gmail.com>
2023-06-16 00:31:08 -0400

    x

--- e_hkbk/b_gift-magic/l_py_ml_usef/z_puzzles/0_sb.py
+++ e_hkbk/b_gift-magic/l_py_ml_usef/z_puzzles/0_sb.py
@@ -1,13 +1,4 @@
-def outer_func(x):
-    y = 1 + x              # 2) x = 4 announced here ; y = 5
-    def inner_func():
-        x = 2               # z) considered, and then rejected 
-        def inner_inner():
-            x = 3           # 3) x = 3
-            return x + y    # 4) 3 + 5 = 8
-        return inner_inner  # 5) ret 8 
-    return inner_func       # 6) ret 8
-
-results = outer_func(4)    # 1) x = 4 ; 7) rets 8 ; 8) results = 8
-my_results = results()
-print(my_results() + 2)    # 9) print(8 + 2) 
+k, v = k[v] = {0: 0}, (0,)
+print(k)
+k, v = k[v] = {0}, (0,)
+print(k)

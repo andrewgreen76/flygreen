95d78d578e76f88f8e05d12b5f737ff0e8d42b3c
Andrew Green <andrewgreen031991@gmail.com>
2023-06-17 06:29:46 -0400

    x

--- e_hkbk/d_be-pro_spzn/5_inline-asm.cpp
+++ e_hkbk/d_be-pro_spzn/5_inline-asm.cpp
@@ -12,3 +12,6 @@ int main() {
 
    return 0;
 }
+
+/*Note that inline Assembly is platform-specific and not part of the standard C++ language. The syntax and available instructions may differ between compilers and architectures. It's also worth mentioning that using inline Assembly should be done sparingly and with caution, as it can make the code less portable and harder to maintain.*/
+

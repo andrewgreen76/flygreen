dc1e60be21cb40aa3e94cf9d126843d48da65ad4
Andrew Green <andrewgreen031991@gmail.com>
2023-06-20 19:08:19 -0400

    x

--- e_hkbk/b_gift-magic/l_py_usef-ml+/m_bind-v-ref.py
+++ e_hkbk/b_gift-magic/l_py_usef-ml+/m_bind-v-ref.py
@@ -1,9 +1,11 @@
 x = [2]
 y = x
+z = x.copy()
 x.append(6)
 
 # What does y hold at this point ?
 # (A) [2]
 # (B) [2, 6]
 
-print(y)
+print(y) # [2, 6]    # This is called a    SHALLOW COPY
+print(z) # [2]       # This is called a    DEEP COPY 

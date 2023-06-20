9d88289306d732ee66410e763053ca8b56cfde12
Andrew Green <andrewgreen031991@gmail.com>
2023-06-20 19:15:34 -0400

    x

--- e_hkbk/b_gift-magic/l_py_usef-ml+/m_kinds-of-copies.py
+++ e_hkbk/b_gift-magic/l_py_usef-ml+/m_kinds-of-copies.py
@@ -11,5 +11,8 @@ x.append(6)
 # (A) [2]
 # (B) [2, 6]
 
-print(y) # [2, 6]    # This is called a    SHALLOW COPY    , which is a copy of the reference to the original object. 
+print(y) # [2, 6]
+# This is called a    SHALLOW COPY    , which is a copy of the reference to the original object.
+# - created using simple name binding (an alias/pointer/name for the same object).
+
 print(z) # [2]       # This is called a    DEEP COPY       , which is a copy of the object itself. 

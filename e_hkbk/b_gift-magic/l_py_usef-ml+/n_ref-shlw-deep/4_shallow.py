bf78ac835460551a774dfef39d2a9a2130348b29
Andrew Green <andrewgreen031991@gmail.com>
2023-06-20 21:50:03 -0400

    x

--- e_hkbk/b_gift-magic/l_py_usef-ml+/n_ref-shlw-deep/4_shallow.py
+++ e_hkbk/b_gift-magic/l_py_usef-ml+/n_ref-shlw-deep/4_shallow.py
@@ -1,14 +1,34 @@
 import copy
 
 original_list = [1, 2, [3]]
+
+print("")
+print("Original list: " + str(original_list))
+print("")
+print("Adding a reference")
+reference = original_list
+print("")
+print("Original list: " + str(original_list))
+print("Reference: " + str(reference))
+print("")
+print("Making a shallow copy")
 shallow_copy = copy.copy(original_list)
-
-# Modify the nested list in the copies
+print("")
+print("Original list: " + str(original_list))
+print("Reference: " + str(reference))
+print("Shallow copy: " + str(shallow_copy))
+print("")
+print("Altering the shallow copy (modifying the nested list in the copy)")
 shallow_copy[2].append(4)
+print("")
+print("Original list: " + str(original_list))
+print("Reference: " + str(reference))
+print("Shallow copy: " + str(shallow_copy))
+print("")
 
 
 
-print("List at genesis: [1, 2, [3]]")
+#print("List at genesis: [1, 2, [3]]")
 #
 #print("Original list became: ")
 #print(original_list)  # [1, 2, [3, 4]]
@@ -19,3 +39,4 @@ print("List at genesis: [1, 2, [3]]")
 
 #print("Deep copy became: ")
 #print(deep_copy)      # [1, 2, [3, 5]]
+

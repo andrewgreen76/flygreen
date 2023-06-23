f69b02d7397a5f3cff3bc01c19903fcf6290b90c
Andrew Green <andrewgreen031991@gmail.com>
2023-06-23 08:47:08 -0400

    x

---
+++ e_hkbk/d_be-pro_spzn/f_c-hwmgmt/n_endian.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+  unsigned int i = 1;    printf("\ni is : %x", i);
+  printf("\nsizeof(unsigned int) : %li", sizeof(unsigned int));
+  printf("\nsizeof(i) : %li\n\n", sizeof(i));
+
+  return 0;
+}

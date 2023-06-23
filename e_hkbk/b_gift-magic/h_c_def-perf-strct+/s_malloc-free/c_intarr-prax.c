0ecca68662210ec41d0c3e72853bb4d1fc8c35fd
Andrew Green <andrewgreen031991@gmail.com>
2023-06-22 23:42:57 -0400

    x

--- e_hkbk/b_gift-magic/h_c_def-perf-strct+/s_malloc-free/c_intarr-prax.c
+++ e_hkbk/b_gift-magic/h_c_def-perf-strct+/s_malloc-free/c_intarr-prax.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  //
+  int iasize; // unknown
+  int * iarr; // will hold addr of array
+
+  printf("\nPlease enter the size for the array of integers you want to create: "); // << 
+  scanf("%i", &iasize); // >> \n
+
+  iarr = (int *)malloc(iasize * sizeof(int));
+
+  if(iarr != NULL) {
+    void* iaddr = (void*)iarr; // void* can hold anything, ESPECIALLY MEMORY ADDRESSES. 
+    printf("Available memory found and allocated at: %p", iaddr); // \n
+
+    free(iarr);
+    printf("Allocated memory released\n\n");
+  }
+  else {
+    printf("Memory unavailable");
+  }
+
+  // Do NOT free here - potential invalid free. 
   
   return 0;
 }
--- e_hkbk/b_gift-magic/h_c_def-perf-strct+/s_malloc-free/e_chararr-prax.c
+++ e_hkbk/b_gift-magic/h_c_def-perf-strct+/s_malloc-free/e_chararr-prax.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  //
+  int casize; // unknown
+  char * carr; // will hold addr of array
+
+  printf("\nPlease enter the size for the array of CHARACTERS you want to create: "); // << 
+  scanf("%i", &casize); // >> \n
+
+  carr = (char *)malloc(casize * sizeof(char));
+
+  if(carr != NULL) {
+    void* caddr = (void*)carr; // void* can hold anything, ESPECIALLY MEMORY ADDRESSES. 
+    printf("Available memory found and allocated at: %p", caddr); // \n
+
+    free(carr);
+    printf("Allocated memory released\n\n");
+  }
+  else {
+    printf("Memory unavailable");
+  }
+
+  // Do NOT free here - potential invalid free. 
   
   return 0;
 }

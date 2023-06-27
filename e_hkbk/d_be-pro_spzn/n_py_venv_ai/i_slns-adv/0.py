9b215de549d8d205e9ab713ab60afe517fa933b8
Andrew Green <andrewgreen031991@gmail.com>
2023-06-27 18:21:39 -0400

    x

---
+++ e_hkbk/d_be-pro_spzn/n_py_venv_ai/i_slns-adv/0.py
@@ -0,0 +1,33 @@
+print("========== How it's NOT done: ==========")
+
+a = [2, 3]
+print(a)
+
+for i in a:
+    print(i)
+    i = i + 2
+    print(i)
+    print("")
+    
+print(a)
+print("")
+print("========== Also how it's NOT done: ==========")
+print("")
+
+a = [2, 3]
+print(a)
+
+for i in a:
+    #print(a[i])
+    #i = i + 2
+
+print("")
+print("========== How it's ACTUALLY done: ==========")
+print("")
+
+a = [2, 3]
+
+for i in range(len(a)):
+    a[i] = a[i] + 2
+
+print(a)

6594b514d9f735e06e08b2a76de2dee4e295a0ac
Andrew Green <andrewgreen031991@gmail.com>
2023-06-30 08:03:24 -0400

    x

---
+++ e_hkbk/b_gift-magic/t_net_dsn_java+/jjj_95_js-web-more/d_oct.js
@@ -0,0 +1,18 @@
+console.log(015 - 015)
+console.log(016 - 015)
+console.log(017 - 015)
+console.log(018 - 015)
+console.log(019 - 015)
+
+// https://twitter.com/swapnakpanda/status/1674266603498745857
+
+/* 
+The mystery lies in the way numbers are interpreted in JavaScript. In JavaScript numbers with a leading zero are treated as octal (base 8) numbers rather than decimal (base 10) numbers
+ */
+
+/* 
+Partially correct.
+
+Only if any of the digits are not 8 or 9. In that case, it's evaluated as a normal decimal.
+*/
+

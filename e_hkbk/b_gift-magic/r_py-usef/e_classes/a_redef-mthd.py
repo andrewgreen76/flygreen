990e5c948d289cf38d93be35649a045af61a2dd4
Andrew Green <andrewgreen031991@gmail.com>
2023-07-04 17:16:27 -0400

    x

---
+++ e_hkbk/b_gift-magic/r_py-usef/e_classes/a_redef-mthd.py
@@ -0,0 +1,10 @@
+class SetAlarm:
+    def run_alarm(self):
+        return "I will run at 10"
+    def run_alarm(self):
+        return "I will run at 11"
+    def run_alarm(self):
+        return "I will run at 12"
+
+time_object = SetAlarm()
+print(time_object.run_alarm())

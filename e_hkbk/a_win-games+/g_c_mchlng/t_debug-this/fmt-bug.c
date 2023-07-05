3f32ea8a9cedfa9b2cdaee15c189e23ca2887f1d
Andrew Green <andrewgreen031991@gmail.com>
2023-07-05 18:48:25 -0400

    x

--- e_hkbk/a_win-games+/g_c_mchlng/t_debug-this/fmt-bug.c
+++ e_hkbk/a_win-games+/g_c_mchlng/t_debug-this/fmt-bug.c
@@ -4,9 +4,9 @@ int main(int argc, char **argv)
 {
   int d = 2;
 
-  printf("Welcome to a program with a bug!\n");
+  printf("\nWelcome to a program with a bug!\n");
   scanf("%d", d);
-  printf("You gave me: %d", d);
+  printf("You gave me: %d\n", d);
 
   return 0;
 }

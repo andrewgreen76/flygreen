e2ee5c5d3dca980794785c5fb07db8cf133ed2df
Andrew Green <andrewgreen031991@gmail.com>
2023-07-05 14:53:02 -0400

    x

--- e_hkbk/b_gift-magic/r_py-usef/6_intelli-make/r_range/0.py
+++ e_hkbk/b_gift-magic/r_py-usef/6_intelli-make/r_range/0.py
@@ -1,8 +1,13 @@
 a = range(3)
 b = range(5)
 
+print(len(a))
+
 x = [[i for i in a] for j in b]
 print(x)
 
 y = [(i, j) for i in a for j in b]
 print(y)
+
+z = [i for i in x*len(a)]
+print(x*len(a))

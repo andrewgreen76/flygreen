332374169dcfd186588127554066b46a266ae196
Andrew Green <andrewgreen031991@gmail.com>
2023-07-06 04:11:52 -0400

    x

--- e_hkbk/a_win-games+/g_c_mchlng/7_basic-progs/i_argc-argv.c
+++ e_hkbk/a_win-games+/g_c_mchlng/7_basic-progs/i_argc-argv.c
@@ -3,11 +3,11 @@
 int main(int argc, char **argv)
 {
   printf("\n");
-
+  printf("\n");
   printf("Your argc is: %i\n", argc);
 
   for(int i=0; i<argc; i++)
-    printf("Your argv[%i] is: %s", i, argv[i]);
+    printf("Your argv[%i] is: %s\n", i, argv[i]);
 
   //printf("Your argv[0] is: %s\n", argv[0]);
   //if(argv[1]) printf("Your argv[1] is: %s\n", argv[1]);

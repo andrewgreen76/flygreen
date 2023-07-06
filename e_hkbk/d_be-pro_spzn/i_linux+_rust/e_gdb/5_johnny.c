938580d26d78f865d76217721f41b4fb58c4f526
Andrew Green <andrewgreen031991@gmail.com>
2023-07-05 23:36:40 -0400

    x

---
+++ e_hkbk/d_be-pro_spzn/i_linux+_rust/e_gdb/5_johnny.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("\n");
+  printf("Actually, this program is supposed to generate a file \"Here's Johnny!\" on your desktop with its name expanding character by character \n");
+  printf("at every equal fraction of a second, with every completion of the spelling followed by renaming the file back to \n");
+  printf("the initial letter of the hardcoded string \"Here's Johnny!\". And this goes on in an infinite loop. :) \n");
+  printf("\n");
+
+  return 0; 
+}

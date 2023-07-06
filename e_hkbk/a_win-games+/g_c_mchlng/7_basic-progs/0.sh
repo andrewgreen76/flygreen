9b652f75a87ab97bd9cb5c81e7d3a75ce16d7eff
Andrew Green <andrewgreen031991@gmail.com>
2023-07-06 04:10:41 -0400

    x

---
+++ e_hkbk/a_win-games+/g_c_mchlng/7_basic-progs/0.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gcc -o run.bx $1 -g
+
+# The bin exec must be run manually in CLI for the user to be able to provide arguments : 
+#./run.bx
---
+++ e_hkbk/a_win-games+/g_c_mchlng/7_basic-progs/i_argc-argv.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  printf("\n");
+
+  printf("Your argc is: %i\n", argc);
+
+  for(int i=0; i<argc; i++)
+    printf("Your argv[%i] is: %s", i, argv[i]);
+
+  //printf("Your argv[0] is: %s\n", argv[0]);
+  //if(argv[1]) printf("Your argv[1] is: %s\n", argv[1]);
+  //strcpy();
+
+  printf("\n");
+  return 0;
+}

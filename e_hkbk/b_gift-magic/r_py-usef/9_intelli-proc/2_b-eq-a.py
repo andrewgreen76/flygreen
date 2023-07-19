9b2d3eff60a388cef7da90bad29da5ff4a621084
Andrew Green <andrewgreen031991@gmail.com>
2023-07-19 07:40:58 -0400

    x

--- e_hkbk/b_gift-magic/r_py-usef/9_intelli-proc/2_b-eq-a.py
+++ e_hkbk/b_gift-magic/r_py-usef/9_intelli-proc/2_b-eq-a.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
-a = [1, 2, 3]    # a -> its own [1, 2, 3]
+a = [1, 2, 3]    # a -> the master list [1, 2, 3]
 print("Committing \"b = a\"")
-b = a    # b -> copy of [1, 2, 3]
+b = a    # b -> the same master list [1, 2, 3]
 print("b -> ", b)
 print("Committing \"a = [4, 5, 6]\"")
-a[0] = 5
+a = [4, 5, 6]   # b still -> the same [1, 2, 3] ; a -> [4, 5, 6] . Re-ref, not mutation. 
 print("b -> ", b)
+
+# If a -> [1, 2, 3] and then a -> [4, 5, 6] , GC would take care of [1, 2, 3].
+

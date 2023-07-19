ad25b215423478f4507653862e428fcd261e0380
Andrew Green <andrewgreen031991@gmail.com>
2023-07-19 06:40:15 -0400

    x

---
+++ e_hkbk/b_gift-magic/r_py-usef/9_intelli-proc/2_b-eq-a.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+a = [1, 2, 3]    # a -> its own [1, 2, 3]
+print("Committing \"b = a\"")
+b = a    # b -> copy of [1, 2, 3]
+print("b -> ", b)
+print("Committing \"a = [4, 5, 6]\"")
+a = [4, 5, 6]
+print("b -> ", b)

9f3a0fefa70263da0e1453af8e59b8254543dd5e
Andrew Green <andrewgreen031991@gmail.com>
2023-08-11 22:08:37 -0700

    x

--- e_hkbk/d_be-pro_spzn/n_py-venv/z_n2ti6_sb/hack_assembler.py
+++ e_hkbk/d_be-pro_spzn/n_py-venv/z_n2ti6_sb/hack_assembler.py
@@ -7,13 +7,17 @@ import parser
 import translator
 import sys
 import os
+
+progname = "prog"
+
 #______________________________________ Function : ARG CHECK ___________________________________________
 def is_arg_good():
-
+    
     if(len(sys.argv) > 1): # "Presence of argument 1" test
         asm_in = sys.argv[1]
         if os.path.exists(asm_in): # "Presence of the file" test
             if(".asm" in asm_in): # "Recognition as .asm" test
+                print(f"Detected file {asm_in}. Processing ... ")
                 return asm_in               
             else:
                 print(f"The file '{asm_in}' is not recognized as an assembly file.")                
@@ -24,33 +28,64 @@ def is_arg_good():
     else:    
         print("Operand assembly file name not provided.")
         return False
-#_________________________________ Function : CREATING .HACK FILE ______________________________________
+#___________________________ Function : FILTERING OUT MULTI-LINE COMMENTS ______________________________    
+def filter_mltln_cmts(prog_asm):
+    comment_start = '/*'
+    comment_end = '*/'
+    no_mltln_asm = progname + '_nml.asm'
+    ignore = False
+    first_line = True
+    
+    with open(prog_asm, 'r') as input_file, open(no_mltln_asm, 'w') as output_file:
+        for line in input_file:    
+            if(not first_line):    
+                output_file.write('\n') # If there's an upcoming line from input file, allow for newline in the output file. 
+            words = line.split()    # THE SEARCH FOR MULTILINE COMMENT BOOKENDS BEGINS. 
+            for word in words:
+                if(word == comment_start):
+                    ignore = True
+                    continue
+                elif(word == comment_end):
+                    ignore = False
+                    continue
+                elif(not ignore):
+                    output_file.write(word)
+            first_line = False
+#___________________________ Function : FILTERING OUT SINGLE-LINE COMMENTS _____________________________    
+def filter_ln_cmts(asm_in):
+    with open(asm_in, 'w') as file:
+        None
+
+def filter_comments(asm_in):
+    filter_mltln_cmts(asm_in)
+    filter_ln_cmts(asm_in)
+#_______________________________ Function : CREATING THE SYMBOL TABLE __________________________________    
+def make_symtable():
+    print("Forgoing the creation of the symbol table ...")
+#__________________________________ Function : CREATING .HACK FILE _____________________________________
 def make_hackfile(asm_in):
-    given_name, the_rest = asm_in.split('.')
-    outfile = given_name + ".hack"
+    outfile = progname + ".hack"
     print("Creating", outfile, "...")
     with open(outfile, 'w') as file:
         None
     return outfile
+############################################ Script body : #############################################
+if(prog_asm := is_arg_good()):               # arg check
+    progname, the_rest = prog_asm.split('.') # extracting the generic program name 
+    filter_comments(prog_asm)                # comment filtration
+    # building symbol table 
     
-#def start_sym_table():
-#    print("Forgoing the creation of the symbol table ...")
-
-########################################### Script body : #############################################
-if(asm_in := is_arg_good()):
-    hack_out = make_hackfile(asm_in)
-    #
-    os.remove(hack_out)
+    #hack_out = make_hackfile(asm_in) # creating .hack file
+    #__________________________________________________________________________________________________
+    # Clean-up : 
+    #os.remove(hack_out)
+    os.remove(progname + '_nml.asm')
+#######################################################################################################    
 
 '''
 - init indep. SYMTBLMGR(.py)
   - build     SYM.TABLE	w/ pre-defs
     as a dict 
-'''
-
-'''
-- handle GIVENNAME.ASM from cli :
-  + outfile = GIVENNAME.HACK
 '''
 
 '''

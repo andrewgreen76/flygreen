bb82891614dcaa497d92a4a10f6b6af4ccda3c94
Andrew Green <andrewgreen031991@gmail.com>
2023-08-11 17:04:20 -0700

    x

--- e_hkbk/d_be-pro_spzn/n_py-venv/z_n2ti6_sb/hkasmr.py
+++ e_hkbk/d_be-pro_spzn/n_py-venv/z_n2ti6_sb/hkasmr.py
@@ -3,20 +3,35 @@
 print()
 print("Starting the Hack Assembler ...")
 
-import symtblmgr
+#import symtblmgr  
 import parser
 import translator
+import sys
 
-print()
+def treat_asm_name():
+    asm_in = sys.argv[1] if len(sys.argv) > 1 else print("\nProvide name of .asm file to process, as in: python3 hkasmr.py __fname__.asm\n")
+    print("Processing", asm_in, "...")
+    given_name, rest = asm_in.split('.')
+    print("Creating", given_name + ".hack")
+
+def start_sym_table():
+    print("Forgoing the creation of the symbol table ...")
+
+treat_asm_name()
+start_sym_table()
 
 '''
 - init indep. SYMTBLMGR(.py)
   - build     SYM.TABLE	w/ pre-defs
     as a dict 
+'''
 
+'''
 - handle GIVENNAME.ASM from cli :
   + outfile = GIVENNAME.HACK
+'''
 
+'''
 - distill ( givenname.asm , givenname_filtmult.txt )
   - purge_mltln_comments : 
     - If first_char = "/*" , then keep parsing until passing "*/"
@@ -24,7 +39,9 @@ print()
   - purge_whlln_comments / purge_inline_comments :
     - use string splitting
     > givenname_filtln.txt
-    
+'''
+
+'''
 - pass_for_LABELS (decls)
   > givenname.hack
 |
@@ -40,3 +57,5 @@ print()
   - delete sym.table
   - delete givenname_filtered.txt
 '''
+
+print()

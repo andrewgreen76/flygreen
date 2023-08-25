987bdc2d6487e3595fe8a3df9aa49c869a62a039
Andrew Green <andrewgreen031991@gmail.com>
2023-08-25 11:50:33 -0700

    x

---
+++ e_hkbk/d_be-pro_spzn/g_c++11-23/1_app/0.sh
@@ -0,0 +1,14 @@
+g++ main.cpp -o zun.out
+
+if test -e zun.out; then
+    echo "Generated binary executable zun.out"
+    echo "Running the program ..."
+    echo
+    ./zun.out
+    echo 
+    echo "End of execution"
+    rm zun.out
+    echo "Binary executable zun.out removed"
+else
+    echo "Failed to generate binary executable zun.out"
+fi

532e13863f21eb69fb6683657a0fcf7de74520ae
Andrew Green <andrewgreen031991@gmail.com>
2023-08-25 18:11:06 -0700

    x

--- p_portfolio/7_ocr-app_c/0.sh
+++ p_portfolio/7_ocr-app_c/0.sh
@@ -3,9 +3,7 @@ gcc main.c ocrmgmt.c -o zun.out -lncurses
 if test -e zun.out; then
     echo "Generated binary executable zun.out"
     echo "Running the program ..."
-    echo
     ./zun.out
-    echo 
     echo "End of execution"
     rm zun.out
     echo "Binary executable zun.out removed"

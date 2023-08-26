307458e9c51b328c77a5b0c36a89792cc7ed7876
Andrew Green <andrewgreen031991@gmail.com>
2023-08-25 20:42:20 -0700

    x

--- p_portfolio/7_ocr-app_c/io_filter.c
+++ p_portfolio/7_ocr-app_c/io_filter.c
@@ -1,14 +1,36 @@
 #include "io_filter.h"
 #include <stdio.h>
+#include <stdlib.h> // for free()
+//#include <string.h>
+///////////////////////////////
+
 
 void get_valid_name(void)
 {
-    printf("Prompting a valid filename ...\n");
-    return;
-}
+  int bad_img_name = 1;
+  char * in_str;
+  size_t size = 0;
+  
+  printf("Prompting a valid filename ...\n\n");
 
+  do {
+    printf("Name of target image file: ");
+    
+    if (getline(&in_str, &size, stdin) != -1) {
+      
+      //bad_img_name = 0;
+    } else {
+        printf("You may be out of available memory.\n");
+    }
+    free(in_str);
+    
+  } while (bad_img_name);
+
+  return;
+}
+//////////////////////////////////////////////////////////////////
 void get_bounded_vals(void)
 {
-    printf("Collecting bounded values ...\n");
-    return;
+  printf("Collecting bounded values ...\n");
+  return;
 }
--- p_portfolio/7_ocr-app_c/ocr_core.c
+++ p_portfolio/7_ocr-app_c/ocr_core.c
@@ -6,4 +6,9 @@
 void run_core_funcs(void)
 {
     printf("Running core OCR and I/O functions ...\n");
+
+    get_valid_name();
+    get_img_props();
+    get_bounded_vals();
+    proc_img();
 }

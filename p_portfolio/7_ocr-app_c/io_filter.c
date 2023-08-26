aacc284cb779a2b86d755a4ca7bba081815e8583
Andrew Green <andrewgreen031991@gmail.com>
2023-08-25 18:43:53 -0700

    x

---
+++ p_portfolio/7_ocr-app_c/io_filter.c
@@ -0,0 +1,7 @@
+#include "io_filter.h"
+#include <stdio.h>
+
+void run_core_funcs(void)
+{
+    printf("Running core OCR and I/O functions ...\n");
+}
---
+++ p_portfolio/7_ocr-app_c/io_filter.h
@@ -0,0 +1,7 @@
+#ifndef IO_FILTER_H
+#define IO_FILTER_H
+
+// Function prototypes
+void run_core_funcs(void);
+
+#endif

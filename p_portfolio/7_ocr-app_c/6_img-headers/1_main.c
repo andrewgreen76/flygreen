b62ba80f3b8696ba080209220e5bcc6b68a78675
Andrew Green <andrewgreen031991@gmail.com>
2023-08-28 15:00:05 -0700

    x

--- p_portfolio/7_ocr-app_c/6_img-headers/1_main.c
+++ p_portfolio/7_ocr-app_c/6_img-headers/1_main.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 
+#pragma pack(push, 1)
+char b;
+#pragma pack(pop)
+
+
+
 int main(int argc, char * argv[])
 {
   printf("\n");
-  char * b;
-
-  char * f = argv[1];
-  char * m = argv[2];
-  printf("char * f : %s\n", f);
-  printf("char * m : %s\n\n", m);
-
-  // scanf() trial :
-  int num;
-  printf("Enter num : ");
-  scanf("%d", &num);
-  printf("You've entered : %d\n", num);
-  
   /*
-  FILE *file_at = fopen(f, m);
-
-  if(file_at) {
-    printf("File %s found\n", f);
-    //    fread(b, sizeof(char), 1, file_at);
-    //printf("%s\n", b);
-  }
+  char * fname = argv[1];
+  char * mode = argv[2];
+  printf("char * fname : %s\n", fname);
+  printf("char * mode: %s\n\n", mode);
   */
+  FILE *f_at = fopen("3.bmp", "rb");
+  if(f_at) {
+    printf("Target file found\n");
+    fread(&b, sizeof(char), 1, f_at);
+    //printf("%s\n", b);
+    fclose(f_at);
+  }
+  else printf("Target file **not** found\n");  
 
   printf("\n");
   return 0;

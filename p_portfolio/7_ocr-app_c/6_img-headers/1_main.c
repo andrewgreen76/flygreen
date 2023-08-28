77063883c172deafbfa5e8af983d34e7c081db33
Andrew Green <andrewgreen031991@gmail.com>
2023-08-28 14:11:23 -0700

    x

--- p_portfolio/7_ocr-app_c/6_img-headers/1_main.c
+++ p_portfolio/7_ocr-app_c/6_img-headers/1_main.c
@@ -3,9 +3,29 @@
 int main(int argc, char * argv[])
 {
   printf("\n");
+  char * b;
 
-  printf("H\n");
+  char * f = argv[1];
+  char * m = argv[2];
+  printf("char * f : %s\n", f);
+  printf("char * m : %s\n\n", m);
+
+  // scanf() trial :
+  int num;
+  printf("Enter num : ");
+  scanf("%d", &num);
+  printf("You've entered : %d\n", num);
   
+  /*
+  FILE *file_at = fopen(f, m);
+
+  if(file_at) {
+    printf("File %s found\n", f);
+    //    fread(b, sizeof(char), 1, file_at);
+    //printf("%s\n", b);
+  }
+  */
+
   printf("\n");
   return 0;
 }

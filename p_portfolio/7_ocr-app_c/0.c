89684a8b1b24f673bbdab459bc66c2e770c34baa
Andrew Green <andrewgreen031991@gmail.com>
2023-08-28 20:28:15 -0700

    x

---
+++ p_portfolio/7_ocr-app_c/0.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("\n");
+  char * i;
+  
+  printf("Give an input : ");
+  scanf("%c", i);
+  //printf("i : %s\n", i);
+  
+  printf("\n");
+  return 0;
+}

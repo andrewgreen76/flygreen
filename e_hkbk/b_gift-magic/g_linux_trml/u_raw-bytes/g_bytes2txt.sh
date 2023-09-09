1a4cf2cdac605040fa1526ab3b752a9776906839
Andrew Green <andrewgreen031991@gmail.com>
2023-09-09 13:08:08 -0700

    x

--- e_hkbk/b_gift-magic/g_linux_trml/u_raw-bytes/g_bytes2txt.sh
+++ e_hkbk/b_gift-magic/g_linux_trml/u_raw-bytes/g_bytes2txt.sh
@@ -2,7 +2,7 @@
 raw_bytes="48656C6C6F20576F726C64"  # This represents "Hello World" in hexadecimal
 
 # Convert the hexadecimal string to binary and write it to a file
-echo -n "$raw_bytes" | xxd -r -p > output.txt
+echo -n "$raw_bytes" | xxd -r -p > m_output.txt
 
 # Read the raw bytes from the file using xxd
-#xxd -p -c 256 output.txt
+#xxd -p -c 256 m_output.txt
--- e_hkbk/b_gift-magic/g_linux_trml/u_raw-bytes/h_b2t-printf.sh
+++ e_hkbk/b_gift-magic/g_linux_trml/u_raw-bytes/h_b2t-printf.sh
@@ -2,4 +2,4 @@
 raw_bytes="72 101 108 108 111 32 87 111 114 108 100"
 
 # Convert the decimal values to characters and write to a file
-printf "$(printf '\\x%x ' $raw_bytes)" > output.txt
+printf "$(printf '\\x%x ' $raw_bytes)" > n_output.txt

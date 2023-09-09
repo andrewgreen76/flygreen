3969a6bd53f604e308bc1f0c9f56260e62ccd93c
Andrew Green <andrewgreen031991@gmail.com>
2023-09-09 13:04:25 -0700

    x

---
+++ e_hkbk/b_gift-magic/g_linux_trml/u_raw-bytes/g_bytes2txt.sh
@@ -0,0 +1,5 @@
+# Define a byte array as hexadecimal values
+raw_bytes="48656C6C6F20576F726C64"  # This represents "Hello World" in hexadecimal
+
+# Convert the hexadecimal string to binary and write it to a file
+echo -n "$raw_bytes" | xxd -r -p > output.txt

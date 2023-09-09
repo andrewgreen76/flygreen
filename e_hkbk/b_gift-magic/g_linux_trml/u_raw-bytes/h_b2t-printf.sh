940a1cca37d8ee95d9a9030bcfd7c53d608bd103
Andrew Green <andrewgreen031991@gmail.com>
2023-09-09 13:07:27 -0700

    x

---
+++ e_hkbk/b_gift-magic/g_linux_trml/u_raw-bytes/h_b2t-printf.sh
@@ -0,0 +1,5 @@
+# Define raw bytes as a sequence of decimal values (example: "72 101 108 108 111 32 87 111 114 108 100")
+raw_bytes="72 101 108 108 111 32 87 111 114 108 100"
+
+# Convert the decimal values to characters and write to a file
+printf "$(printf '\\x%x ' $raw_bytes)" > output.txt

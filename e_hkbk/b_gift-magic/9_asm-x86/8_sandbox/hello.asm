01233827f66aeec99fa6c0e44a8c2fb7039f6590
Andrew Green <andrewgreen031991@gmail.com>
2023-09-11 00:35:20 -0700

    x

--- e_hkbk/b_gift-magic/9_asm-x86/8_sandbox/hello.asm
+++ e_hkbk/b_gift-magic/9_asm-x86/8_sandbox/hello.asm
@@ -1,14 +1,21 @@
-global main
 section .data
-   msg: db "Hello, World!",0xa
-   len: equ $-msg
+    hello db 'Hello, Assembly!',0
+    hello_len equ $ - hello
+
 section .text
+    global main
+
 main:
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, msg
-   mov edx, len
-   int 0x80
-   mov al, 1
-   mov ebx, 0
-   int 0x80
+    ; Prepare arguments for the write system call
+    mov eax, 4         ; syscall number for sys_write (4)
+    mov ebx, 1         ; file descriptor for stdout (1)
+    mov ecx, hello     ; pointer to the message
+    mov edx, hello_len ; length of the message
+
+    ; Invoke the write system call
+    int 0x80
+
+    ; Exit the program
+    mov eax, 1         ; syscall number for sys_exit (1)
+    xor ebx, ebx       ; exit status (0)
+    int 0x80

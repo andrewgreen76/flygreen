4c41062309ef550733d05031b3bb69cab903db34
Andrew Green <andrewgreen031991@gmail.com>
2023-09-18 13:24:59 -0700

    x

--- e_hkbk/b_gift-magic/e_linux_trml/2_bash_cmds/r_eval-as-cmd.sh
+++ e_hkbk/b_gift-magic/e_linux_trml/2_bash_cmds/r_eval-as-cmd.sh
@@ -1,2 +1,19 @@
 a=`cat l_auto_add.sh | head -6 | tail -1`
 eval $a
+
+#!/bin/bash
+
+# File containing commands, one per line
+file="commands.txt"
+
+# Check if the file exists
+if [ -e "$file" ]; then
+  # Read each line from the file
+  while read -r line; do
+    # Run the line as a command using eval
+    eval "$line"
+  done < "$file"
+else
+  echo "File '$file' does not exist."
+fi
+

a6ee9b42d177778040b904a3b71742f331a95f0c
Andrew Green <andrewgreen031991@gmail.com>
2023-10-19 08:17:42 -0700

    x

--- e_hkbk/a_win-games+/i_c_mchlng/3_sb.c
+++ e_hkbk/a_win-games+/i_c_mchlng/3_sb.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main(){
-  printf("%lf\n", &a);
+  int addr_int = 1234;
+  //char addr_char = 0xff;
+  
+  printf("\n%x\n\n", addr_int >> 8);
+  printf("\n%x\n\n", addr_int );
+  //printf("\n%i\n\n", (int)0x7fffffff );
+  //printf("\n%i\n\n", (int)0x7fffffff + 1 );
 }

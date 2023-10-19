3a6a313a073cc04969373cf88565a5560634f0f6
Andrew Green <andrewgreen031991@gmail.com>
2023-10-19 15:44:35 -0700

    x

---
+++ e_hkbk/b_gift-magic/8_c_perfce-strc+/0_sb.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+double m2ft(double m) {return m*3.2808;}
+double g2lbs(double g) {return g*0.002205;}
+double c2fh(double c) {return c*1.8+32;}
+
+int main(void){
+  printf("\n%d\n\n", (int)0x7fffffff+1);
+}

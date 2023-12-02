943ba1aaa4580c362dc7d19fc5350c4cce3d76e5
Andrew Green <andrewgreen031991@gmail.com>
2023-12-01 19:07:09 -0800

    x

--- e_hkbk/d_be-pro_spzn/5_c-emb/c_c-stkmgmt/x_embc/n_endian-2.c
+++ e_hkbk/d_be-pro_spzn/5_c-emb/c_c-stkmgmt/x_embc/n_endian-2.c
@@ -11,14 +11,27 @@ int main(){
   unsigned int i = 1;   // Ensure : MSB is not a sign bit, LSB holds 0000-0001 : 
                         // 0x   00 00 00 01
                         //      0000 0000 0000 0000    0000 0000 0000 0001
+                        // &i = 0x 7ffc551658d0
 
   printf("&i : %p\n", &i );
-  printf("(char *) &i : %p\n", (char *) &i ); 
+  printf("(char *) &i : %p\n", (char *) &i );
+  
   /*
   char * c = (char *) &i; 
 
   if(*c) printf("Little-endian\n");
   else printf("Big-endian\n");
   */
+
+  int g = 0x00000001;   // g = 00 00 00 01
+                        //              ^ 
+                        // &g = 0x 7ffc551658d0
+  //printf("&g : %p", &g );
+  //char j = &g;
+  //printf("Out : %s", (char *) &g );
+  //(char) &g  // Take the var.address
+  //int *h = &g;
+  //printf("Out : %d", *h);
+
   printf("\n");
 }

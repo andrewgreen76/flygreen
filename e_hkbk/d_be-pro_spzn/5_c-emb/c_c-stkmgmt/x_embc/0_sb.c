6cfb024d818bc2f4f58416afb0eddd5f55b43e40
Andrew Green <andrewgreen031991@gmail.com>
2023-12-02 11:32:17 -0800

    x

--- e_hkbk/d_be-pro_spzn/5_c-emb/c_c-stkmgmt/x_embc/0_sb.c
+++ e_hkbk/d_be-pro_spzn/5_c-emb/c_c-stkmgmt/x_embc/0_sb.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
 
 int main() {
-    int value = 10;
-    
-    // Invalid shift by a value greater than or equal to the width of int
-    int invalidShift = value << 32;  // Shift by 32 bits
-    
-    printf("Invalid shift result: %d\n", invalidShift);
-    
-    return 0;
+  printf("\n");
+  
+  int a = 1, b = 2, c;
+
+  printf("a = %d\n", a);
+  printf("b = %d\n", b);
+  printf("\n");
+  /*
+  c = a;
+  a = b;
+  b = c;
+  */
+
+  /*
+    a = 0001
+    b = 0010
+
+    a = 0011
+    b = 0010
+
+    a = 0011
+    b = 0001
+
+    a = 0010
+    b = 0001
+   */
+  
+  a ^= b ^= a ^= b;
+  
+  printf("a = %d\n", a);
+  printf("b = %d\n", b);
+  
+  printf("\n");
 }

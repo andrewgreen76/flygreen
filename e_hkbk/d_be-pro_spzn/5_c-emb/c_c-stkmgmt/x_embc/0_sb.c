197bf1d4b5a577356b48444d6804df0d12d6134c
Andrew Green <andrewgreen031991@gmail.com>
2023-12-02 15:39:29 -0800

    x

--- e_hkbk/d_be-pro_spzn/5_c-emb/c_c-stkmgmt/x_embc/0_sb.c
+++ e_hkbk/d_be-pro_spzn/5_c-emb/c_c-stkmgmt/x_embc/0_sb.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-int main() {
-  printf("\n");
-
-  int a[3] = {4,3}; 
-
-  for(int i=0; i<3; i++) printf("Out : %d\n", a[i] );
-  
-  printf("\n");
+int main(){
+    const int ws = 68, maxls = 40;
+    char text[ws][maxls];
+    int w;
+    
+    for(w=0; w<ws; w++) scanf("%s", text[w] ) ;    
+    for(w= ws-1; w>-1; w--) printf("%s ", text[w] ) ;
 }

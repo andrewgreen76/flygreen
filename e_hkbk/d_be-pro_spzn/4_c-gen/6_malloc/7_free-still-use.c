53a2ac912fd9b4fb6a4027fccb3724ec4c060565
Andrew Green <andrewgreen031991@gmail.com>
2023-12-02 20:04:26 -0800

    x

--- e_hkbk/d_be-pro_spzn/4_c-gen/6_malloc/7_free-still-use.c
+++ e_hkbk/d_be-pro_spzn/4_c-gen/6_malloc/7_free-still-use.c
@@ -1,3 +1,8 @@
+/*
+  Even though the heap chunk w/ ints is "released", the ints are still there and the mem.addr is still known. 
+  Nevertheless, the chunk is <MARKED> AS REUSABLE and the ints can be overwritten. 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 

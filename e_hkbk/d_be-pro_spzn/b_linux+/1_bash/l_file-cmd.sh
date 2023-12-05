a3f143adb4f2d33e478e6e402d2de4ba5c5de425
Andrew Green <andrewgreen031991@gmail.com>
2023-12-04 19:49:43 -0800

    x

--- e_hkbk/d_be-pro_spzn/b_linux+/1_bash/l_file-cmd.sh
+++ e_hkbk/d_be-pro_spzn/b_linux+/1_bash/l_file-cmd.sh
@@ -2,6 +2,6 @@
 
 # Prints info about the type of the target file. 
 
-file $1
+file [file]
 
 # 0_run: ELF 64-bit LSB pie executable, x86-64, version 1 (SYSV), dynamically linked, interpreter /lib64/ld-linux-x86-64.so.2, BuildID[sha1]=851292b47c24540961601711231268ddab0a7de3, for GNU/Linux 3.2.0, stripped

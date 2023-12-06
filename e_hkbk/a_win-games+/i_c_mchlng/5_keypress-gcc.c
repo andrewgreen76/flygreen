0ad79f3a74d0a1b55626a735534fd8af27a1922a
Andrew Green <andrewgreen031991@gmail.com>
2023-12-05 19:39:02 -0800

    x

--- e_hkbk/a_win-games+/i_c_mchlng/5_keypress-gcc.c
+++ e_hkbk/a_win-games+/i_c_mchlng/5_keypress-gcc.c
@@ -6,6 +6,8 @@ int main() {
     struct termios old_termios, new_termios;
     char c;
 
+    printf("\n");
+    
     // Save current terminal settings
     tcgetattr(STDIN_FILENO, &old_termios);
     new_termios = old_termios;
@@ -14,13 +16,15 @@ int main() {
     new_termios.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
 
-    printf("Press any key to continue...\n");
+    printf("Press any key for it to be identified: ");
     c = getchar(); // Read a single key
 
     // Restore original terminal settings
     tcsetattr(STDIN_FILENO, TCSANOW, &old_termios);
 
-    printf("You pressed: %c\n", c);
+    printf("\nYou pressed: %c\n", c);
+
+    printf("\n");
 
     return 0;
 }

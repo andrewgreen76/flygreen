d2aad0d402f281ff423b382225064ebc4145e459
Andrew Green <andrewgreen031991@gmail.com>
2023-12-05 19:44:45 -0800

    x

---
+++ e_hkbk/b_gift-magic/8_c_perfce-strc+/3_io_canon_awfcase-getch/9_getchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  printf("\n");
+  int c;
+
+  printf("Enter anything: ");
+  c = getchar();
+  printf("Output: %c\n", c);
+  
+  printf("\n");  
+  return 0;
+}

5a9e283683997f5efd635ea18ef7f87ac8436cac
Andrew Green <andrewgreen031991@gmail.com>
2023-12-06 19:46:43 -0800

    x

--- e_hkbk/d_be-pro_spzn/4_c-gen/0.c
+++ e_hkbk/d_be-pro_spzn/4_c-gen/0.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
 
 int main(){
-	char familyName[50];
-	char firstName[50];
-	int age;
-	printf("What is your family name?");
-	fflush(stdout);
-	scanf("%s", familyName);
-	printf("What is your first name?");
-	fflush(stdout);
-	scanf("%s", firstName);
-	printf("What is your age?");
-	fflush(stdout);
-	scanf("%d", &age);
-	printf("%s %s %d\n", familyName,firstName,age);
-	return 0;
+  printf("\n");
+  printf("Haha");
+  printf("\n");
 }

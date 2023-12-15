111cce04c9f6c0458b4656127029f00fe0180c53
Andrew Green <andrewgreen031991@gmail.com>
2023-12-14 18:11:51 -0800

    x

--- e_hkbk/b_gift-magic/d_vms-emus/6_vm/5_virt-ex/0_run-all.sh
+++ e_hkbk/b_gift-magic/d_vms-emus/6_vm/5_virt-ex/0_run-all.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 
+# Exectuting multiple instances of the same infinite-loop process with different arguments : 
+
 ./a.out "John" & ./a.out "Paul" & ./a.out "George" & ./a.out "Ringo" &
 
 # Ctrl+C           to kill the sole currently running process 

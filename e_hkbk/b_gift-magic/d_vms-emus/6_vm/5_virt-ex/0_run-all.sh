3fcf6fbe5e310c32bb1a8541be34cdc1a91880d1
Andrew Green <andrewgreen031991@gmail.com>
2023-12-14 17:18:57 -0800

    x

--- e_hkbk/b_gift-magic/d_vms-emus/6_vm/5_virt-ex/0_run-all.sh
+++ e_hkbk/b_gift-magic/d_vms-emus/6_vm/5_virt-ex/0_run-all.sh
@@ -1,3 +1,6 @@
 #!bin/bash
 
-./cpu "One" & ./cpu "Two" & ./cpu "Three" & ./cpu "Four" &
+./a.out "John" & ./a.out "Paul" & ./a.out "George" & ./a.out "Ringo" &
+
+# Ctrl+C           to kill the sole currently running process 
+# pkill a.out      to kill all processes running the same executable under the same name 

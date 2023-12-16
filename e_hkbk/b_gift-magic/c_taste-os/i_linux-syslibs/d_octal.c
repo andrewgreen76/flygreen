b27c80f93f6dbcbffef87dd160f9767f7ed8233f
Andrew Green <andrewgreen031991@gmail.com>
2023-12-15 17:58:58 -0800

    x

---
+++ e_hkbk/b_gift-magic/c_taste-os/i_linux-syslibs/d_octal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define G	00000107
+#define H	00000110
+#define I	00000111
+#define J	00000112
+
+int main(){
+  printf("\n");
+
+  // for(int i=0; i<20; i++)     printf("53 | %d : %c\n", i, 53 | i);
+  printf(": %c\n", G);
+  printf(": %c\n", G+1);
+  printf(": %c\n", G+2);
+  printf(": %c\n", G+3);
+
+  printf("\n");
+}
+
+//

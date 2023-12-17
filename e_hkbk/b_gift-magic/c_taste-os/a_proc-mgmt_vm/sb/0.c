50b8c640bce7e21a28b797ebac1c8b77433677d4
Andrew Green <andrewgreen031991@gmail.com>
2023-12-17 14:43:49 -0800

    x

--- e_hkbk/b_gift-magic/c_taste-os/a_proc-mgmt_vm/sb/0.c
+++ e_hkbk/b_gift-magic/c_taste-os/a_proc-mgmt_vm/sb/0.c
@@ -2,9 +2,18 @@
 #include <unistd.h>
 
 int main(){
+  /*
   int pcode = fork();
   
   if(pcode<0) printf("Failed to create a child process\n");
   else if(pcode==0) printf("I am child process : %d\n", (int)getpid() );
   else printf("I am parent process : %d\n", (int)getpid() );
+  */
+
+  int a = -245 ;
+
+  printf("\n");
+  if(a) printf("YES \n");
+  else printf("NO \n");
+  printf("\n");
 }

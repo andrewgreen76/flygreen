979b8cb457136cee5c8f0a3fdd311c34210a6afa
Andrew Green <andrewgreen031991@gmail.com>
2023-12-17 17:26:02 -0800

    x

--- e_hkbk/b_gift-magic/c_taste-os/c_proc-mgmt_vm/sb/0.c
+++ e_hkbk/b_gift-magic/c_taste-os/c_proc-mgmt_vm/sb/0.c
@@ -1,27 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include <string.h>
 
 int
 main(int argc, char *argv[])
 {
-    printf("Starting the program... (pid:%d)\n", (int) getpid());
-    int child = fork();
-    if (child < 0) {
-        // child process failed, exit the program
-        fprintf(stderr, "Child process was not created\n");
-        exit(1);
-    } else if (child != 0) {
-        // parent process runs this branch
-        int waiting = wait(NULL);
-        printf("Hello from the parent process (waiting:%d) (pid:%d)\n",
-               waiting, (int) getpid());
-    } else if (child == 0) {
-        // child process created successfully
-        printf("Hello from the child process (pid:%d)\n", (int) getpid());
-        sleep(5);
-    } 
-    
-    return 0;
+  char *exampleargs[3];
+  exampleargs[0] = strdup("wc");   // calls the word count program
 }

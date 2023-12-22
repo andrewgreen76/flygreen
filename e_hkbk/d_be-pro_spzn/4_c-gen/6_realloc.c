5f7915873ab1e2b43b50802ec0ad0f25b28c37ae
Andrew Green <andrewgreen031991@gmail.com>
2023-12-22 13:01:18 -0800

    x

--- e_hkbk/d_be-pro_spzn/4_c-gen/6_realloc.c
+++ e_hkbk/d_be-pro_spzn/4_c-gen/6_realloc.c
@@ -7,32 +7,29 @@
 
 int main(int argc, char *argv[]) {
   printf("\n");
-
   int i;
+  // ==================== CALLOC ====================
   int *arr = calloc(sizeof(int), 5);
   //
   for (i = 0; i < 5; i++) {
     printf("[%d]", arr[i]);
   }
   printf("\n");
-
-  
+  // ==================== REALLOC ====================
   arr = realloc(arr, sizeof(int) * 10);
   //
   for (i = 0; i < 10; i++) {
     printf("[%d]", arr[i]);
   }
   printf("\n");
-
-
+  // ==================== REALLOC ====================
   arr = realloc(arr, sizeof(int) * 3);
   //
   for (i = 0; i < 3; i++) {
     printf("[%d]", arr[i]);
   }
   printf("\n");
-
-  
+  // ==================== THE END ====================  
   free(arr);
   printf("\n");
 }

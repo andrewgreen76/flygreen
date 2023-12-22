7c6432b48e50adaa4c9ea5358f1258caf6b3ed4c
Andrew Green <andrewgreen031991@gmail.com>
2023-12-21 17:24:08 -0800

    x

--- e_hkbk/d_be-pro_spzn/4_c-gen/5_malloc-free_why/w_stat32-dyn64.c
+++ e_hkbk/d_be-pro_spzn/4_c-gen/5_malloc-free_why/w_stat32-dyn64.c
@@ -1,3 +1,11 @@
+/*
+  sizeof(int_ptr) = 8 bytes 
+  sizeof(int) = 4 bytes 
+
+  Statically allocating memory for an int charges FOUR bytes on the stack (as though your machine is 32-bit). 
+  Dynamically allocating memory for an int charges EIGHT bytes on the heap (as though your machine is 64-bit).
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,10 +17,14 @@ int main(){
   int *y = (int * )malloc( sizeof(int) );
   int *z = (int * )malloc( 10*sizeof(int) );
   
-  printf("%ld\n", sizeof(w) );
-  printf("%ld\n", sizeof(x) );
-  printf("%ld\n", sizeof(y) );
-  printf("%ld\n", sizeof(z) );
+  printf("sizeof(int) = %ld\n", sizeof(w) );
+  printf("sizeof(int_arr) = %ld\n", sizeof(x) );
+  printf("\n");
+  printf("sizeof(int_ptr y) = %ld\n", sizeof(y) );
+  printf("sizeof(deref'd y) = %ld\n", sizeof(*y) );
+  printf("\n");
+  printf("sizeof(intarr_ptr z) = %ld\n", sizeof(z) );
+  printf("sizeof(deref'd z ptr to the first of ten ints) = %ld\n", sizeof(*z) );
   
   printf("\n");
 }

38bcc9f1da201fdc302e011f3137726c687d3325
Andrew Green <andrewgreen031991@gmail.com>
2023-12-22 13:58:36 -0800

    x

--- e_hkbk/b_gift-magic/8_c_perfce-strc+/c_str-lib/7_strcat.c
+++ e_hkbk/b_gift-magic/8_c_perfce-strc+/c_str-lib/7_strcat.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 int main () {
    printf("\n");
@@ -9,11 +10,14 @@ int main () {
 
    txt = (char *) malloc(6);
    strcpy(txt, "Hello");
-   printf("String: %s,  Address: %u\n", txt, txt);
+   printf("String: %s,  Address: %lu\n", txt, (uint64_t) txt);
 
    txt = (char *) realloc(txt, 25);
    strcat(txt, " World");
-   printf("String: %s,  Address: %u\n", txt, txt);
+   printf("String: %s,  Address: %lu\n", txt, (uint64_t) txt);
+
+   free(txt);
+   //printf("String: %s,  Address: %u\n", txt, txt);
 
    printf("\n");
 }

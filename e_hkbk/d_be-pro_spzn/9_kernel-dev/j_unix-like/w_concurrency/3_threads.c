abe7b9060460c13c047137ece5226f93232c2a4d
Andrew Green <andrewgreen031991@gmail.com>
2024-01-02 21:23:54 -0800

    x

--- e_hkbk/d_be-pro_spzn/9_kernel-dev/j_unix-like/w_concurrency/3_threads.c
+++ e_hkbk/d_be-pro_spzn/9_kernel-dev/j_unix-like/w_concurrency/3_threads.c
@@ -1,38 +1,30 @@
- #include <stdio.h>
- #include <pthread.h>
- #include "common.h"
- #include "common_threads.h"
-
- static volatile int counter = 0;
+#include <stdio.h>
+#include <assert.h>
+#include <pthread.h>
 
 
- // Simply adds 1 to counter repeatedly, in a loop
-  //
- void *mythread(void *arg) {
-    printf("%s: begin\n", (char *) arg);
-        int i;
-        for (i = 0; i < 1e7; i++) {
-        counter = counter + 1;
-    }
-    printf("%s: done\n", (char *) arg);
-    return NULL;
- }
+void *mythread(void *arg) {
+  printf("%s\n", (char *) arg);
+  return NULL;
+}
 
- // main()
- //
- // Just launches two threads (pthread_create)
- // and then waits for them (pthread_join)
- //
- int main(int argc, char *argv[]) {
-    pthread_t p1, p2;
-    printf("main: begin (counter = %d)\n", counter);
-    Pthread_create(&p1, NULL, mythread, "A");
-    Pthread_create(&p2, NULL, mythread, "B");
 
-    // join waits for the threads to finish
-    Pthread_join(p1, NULL);
-    Pthread_join(p2, NULL);
-    printf("main: done with both (counter = %d)\n",
-    counter);
+int main(int argc, char *argv[]) {
+    
+    pthread_t thread1, thread2;
+
+    int rc;
+    printf("In main before creating threads\n");
+
+    // create two threads
+    pthread_create(&thread1, NULL, mythread, "Thread1");
+    pthread_create(&thread2, NULL, mythread, "Thread2");
+
+    // wait for the threads to complete
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    printf("In main after running threads\n");
+
     return 0;
- }
+}

615222c343cbb4ea72e603b6905d7a4078c9046a
Andrew Green <andrewgreen031991@gmail.com>
2024-01-05 17:12:24 -0800

    x

--- e_hkbk/d_be-pro_spzn/9_k-dev/j_unix-like/w_conc/7_thread-api/7_pthread-join.c
+++ e_hkbk/d_be-pro_spzn/9_k-dev/j_unix-like/w_conc/7_thread-api/7_pthread-join.c
@@ -1,22 +1,34 @@
+/*
+  "We normally create multiple threads and wait for them to finish" instead of using pthread_join(). 
+  . "To ensure that all work is completed before departing or going on to the next step of computation, 
+     a parallel program may typically employ join."
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
 typedef struct { int a; int b; } myarg_t;
 typedef struct { int x; int y; } myret_t;
 
 void *mythread(void *arg) {
-    myret_t *rvals = Malloc(sizeof(myret_t));
-    rvals->x = 1;
-    rvals->y = 2;
-    return (void *) rvals;
+    myarg_t *args = (myarg_t *) arg;
+    printf("%d %d\n", args->a, args->b);
+    myret_t oops; // ALLOCATED ON STACK: BAD!
+    oops.x = 1;
+    oops.y = 2;
+    return (void *) &oops;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t p;
     myret_t *rvals;
     myarg_t args = { 10, 20 };
-    Pthread_create(&p, NULL, mythread, &args); /* 4th para reflects the arg of thread_func
+    pthread_create(&p, NULL, mythread, &args); /* 4th para reflects the arg of thread_func
 						  . can be NULL, but then : thread_func(VOID * arg) 
 						*/
     
-    Pthread_join(p, (void **) &rvals);         /* 2nd para reflects the ret type of thread_func
+    pthread_join(p, (void **) &rvals);         /* 2nd para reflects the ret type of thread_func
 						  . can be NULL, but then : VOID * thread_func(void * arg)
 						*/
     printf("returned %d %d\n", rvals->x, rvals->y);

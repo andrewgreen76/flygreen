9b9698883a401116b3b0aa676926b945b95ba6a2
Andrew Green <andrewgreen031991@gmail.com>
2024-01-07 12:16:19 -0800

    x

--- e_hkbk/d_be-pro_spzn/9_k-dev/j_unix-like/w_conc/9_locks/g_test-and-set.c
+++ e_hkbk/d_be-pro_spzn/9_k-dev/j_unix-like/w_conc/9_locks/g_test-and-set.c
@@ -11,8 +11,16 @@ int TestAndSet(int *old_ptr, int new) {                           //            
     *old_ptr = new;     // 101 : store ’new’ into old_ptr         ->   101 : flag <- 1         ->   100 : load ret flag<>1 
     return old; 	// 102 : return the old value             ->   102 : RET 1 (ATOMIZE!!) ->   101 : flag <- 1 
 }                       //                                                                     ->   102 : ret
-
-
+//
+//
+//                                                                // THREAD 1 :                // THREAD 2 :
+int atomic_TestAndSet(int *old_ptr, int new) {                    //                (flag=0 , new=1)   
+    int old = *old_ptr; // 100 : fetch old value at old_ptr       ->   100 : load ret flag<>0  ->    
+    *old_ptr = new;     // 101 : store ’new’ into old_ptr         ->   101 : flag <- 1         ->    
+    return old; 	// 102 : return the old value             ->   102 : ret 0             ->    
+}                                                                 //                           ->   100 : load ret flag<>1
+                                                                  //                           ->   101 : flag <- 1
+                                                                  //                           ->   102 : ret 1
 void init(lock_t *lock) {
     // 0: lock is available, 1: lock is held
     lock->flag = 0;

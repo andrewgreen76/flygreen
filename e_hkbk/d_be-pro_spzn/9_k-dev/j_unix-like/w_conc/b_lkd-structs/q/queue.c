5ea39934b1d9217bfa6357d57cf219e14d314905
Andrew Green <andrewgreen031991@gmail.com>
2024-01-10 14:20:42 -0800

    x

--- e_hkbk/d_be-pro_spzn/9_k-dev/j_unix-like/w_conc/b_lkd-structs/q/queue.c
+++ e_hkbk/d_be-pro_spzn/9_k-dev/j_unix-like/w_conc/b_lkd-structs/q/queue.c
@@ -14,30 +14,39 @@ void queue_init(queue_t *q) {
 }
 
 
+// this Q impl :
+//  . head->node->...->node->tail ,    not    head<-node<-...<-node<-tail
+//  . insert at the tail 
 void queue_enqueue(queue_t *q, int value) {
     node_t *tmp = malloc(sizeof(node_t));
-    assert(tmp != NULL);
+    assert(tmp != NULL); // abort if malloc failed 
     tmp->value = value;
     tmp->next = NULL;
 
     pthread_mutex_lock(&q->tail_lock);
-    q->tail->next = tmp;
-    q->tail = tmp;
+    q->tail->next = tmp; // tie new node to old tail 
+    q->tail = tmp;       // announce new node as new tail 
     pthread_mutex_unlock(&q->tail_lock);
 }
 
 
+// this Q impl :
+//  . head->node->...->node->tail ,    not    head<-node<-...<-node<-tail
+//  . remove at the head
+//  . rets SUCCESS / FAILURE
 int queue_dequeue(queue_t *q, int *value) {
-    pthread_mutex_lock(&q->head_lock);
-    node_t *tmp = q->head;
-    node_t *new_head = tmp->next;
-    if (new_head == NULL) {
-    pthread_mutex_unlock(&q->head_lock);
-    return -1; // queue was empty
-    }
-    *value = new_head->value;
+    pthread_mutex_lock(&q->head_lock); 
+    node_t *tmp = q->head;                        // head -> (nnnn) <- tmp
+    node_t *new_head = tmp->next;                 //           |
+                                                  //           v
+    if (new_head == NULL) {                       //  new -> NEW_HD
+	pthread_mutex_unlock(&q->head_lock);      //           |
+	return -1; // queue was empty             //           v 
+    }                                             
+    
+    *value = new_head->value; // GET NEW_HEAD->VAL VIA ARG 
     q->head = new_head;
     pthread_mutex_unlock(&q->head_lock);
-    free(tmp);
+    free(tmp); // now remove old head 
     return 0;
 }

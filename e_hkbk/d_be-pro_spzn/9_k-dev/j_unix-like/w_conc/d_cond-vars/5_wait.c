a5590888fb7f583e0534e56f63efed92671afd7b
Andrew Green <andrewgreen031991@gmail.com>
2024-01-12 16:55:06 -0800

    x

--- e_hkbk/d_be-pro_spzn/9_k-dev/j_unix-like/w_conc/d_cond-vars/5_wait.c
+++ e_hkbk/d_be-pro_spzn/9_k-dev/j_unix-like/w_conc/d_cond-vars/5_wait.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <pthread.h>
 
-volatile int done = 0;
-
 void *child(void *arg) {
     printf("child\n");
-    done = 1;
+    // XXX how to indicate we are done?
     return NULL;
 }
 
@@ -13,11 +11,8 @@ void *child(void *arg) {
 int main(int argc, char *argv[]) {
     printf("parent: begin\n");
     pthread_t c;
-    pthread_create(&c, NULL, child, NULL); // create child
-    
-    while (done == 0)
-    ; // spin
-    
+    Pthread_create(&c, NULL, child, NULL); // create child
+    // XXX how to wait for child?
     printf("parent: end\n");
     return 0;
 }

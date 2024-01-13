5b9eacf084f38080fb7914d15eb0855f27d82105
Andrew Green <andrewgreen031991@gmail.com>
2024-01-12 16:51:58 -0800

    x

---
+++ e_hkbk/d_be-pro_spzn/9_k-dev/j_unix-like/w_conc/d_cond-vars/3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <pthread.h>
+
+volatile int done = 0;
+
+void *child(void *arg) {
+    printf("child\n");
+    done = 1;
+    return NULL;
+}
+
+
+int main(int argc, char *argv[]) {
+    printf("parent: begin\n");
+    pthread_t c;
+    pthread_create(&c, NULL, child, NULL); // create child
+    
+    while (done == 0)
+    ; // spin
+    
+    printf("parent: end\n");
+    return 0;
+}

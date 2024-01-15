aa11a886fab90f00d86b439d98f1174bd06c2921
Andrew Green <andrewgreen031991@gmail.com>
2024-01-14 22:46:28 -0800

    x

--- e_hkbk/d_be-pro_spzn/9_k-dev/j_unix-like/w_conc/e_sem/s_impl.c
+++ e_hkbk/d_be-pro_spzn/9_k-dev/j_unix-like/w_conc/e_sem/s_impl.c
@@ -5,7 +5,7 @@ typedef struct __Zem_t {
 } Zem_t;
 
 
-// only one thread can call this
+// only one thread can call this (one semaphore/init for all threads) 
 void Zem_init(Zem_t *s, int value) {
   s->value = value;
   Cond_init(&s->cond);
@@ -13,18 +13,36 @@ void Zem_init(Zem_t *s, int value) {
 }
 
 
+// alternative sem_wait() - wait-then-decr 
 void Zem_wait(Zem_t *s) {
   Mutex_lock(&s->lock);
   while (s->value <= 0)
     Cond_wait(&s->cond, &s->lock);
-  s->value--;
+  s->value--;   // 1st thread skips
+                // sem-val will never be negative 
   Mutex_unlock(&s->lock);
 }
+// ... vs. decr-then-wait
+/*
+  This looks like a true binary semaphore : 
+   . 1 - free 
+   . 0 - acquired (whether or not there is another thread coming through) 
+     . T_x is done -> raises sem-val 
+     -> sounds off to a waiting thread T_y 
+     -> awoken thread lowers sem-val immediately 
+     -> ... crit_sect ... 
+*/
 
 
 void Zem_post(Zem_t *s) {
   Mutex_lock(&s->lock);
-  s->value++;
-  Cond_signal(&s->cond);
+  s->value++;            // Tn raises sem-val 
+  Cond_signal(&s->cond); // -> sounds off to a waiting thread 
   Mutex_unlock(&s->lock);
 }
+
+/*
+  s    P    C
+
+  5    //   // 
+*/

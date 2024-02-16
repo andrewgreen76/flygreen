eeaf36ffccff873b4b865a2663fc505786340282
Andrew Green <andrewgreen031991@gmail.com>
2024-02-16 08:33:54 -0800

    x

--- e_hkbk/d_be-pro_spzn/d_k-dev/j_unix-like/w_perst/9_fs/m_dup2.c
+++ e_hkbk/d_be-pro_spzn/d_k-dev/j_unix-like/w_perst/9_fs/m_dup2.c
@@ -6,12 +6,9 @@ int main() {
     int dest_fd = open("example.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
     // Redirect standard output to the file descriptor 'file'
-    //    dup2(STDOUT_FILENO , file );
     dup2( dest_fd , STDOUT_FILENO );
-    //    dup2(file, STDOUT_FILENO);
-    // Now, anything written to STDOUT_FILENO will be directed to 'output.txt'
     printf("This goes to the file.\n");
 
-    close(file);
+    close(dest_fd);
     return 0;
 }

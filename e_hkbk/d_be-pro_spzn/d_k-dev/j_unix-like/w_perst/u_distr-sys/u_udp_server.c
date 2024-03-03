b33d66624fafce203267d0d2d383d4e6432e6882
Andrew Green <andrewgreen031991@gmail.com>
2024-03-02 21:53:20 -0800

    x

--- e_hkbk/d_be-pro_spzn/d_k-dev/j_unix-like/w_perst/u_distr-sys/u_udp_server.c
+++ e_hkbk/d_be-pro_spzn/d_k-dev/j_unix-like/w_perst/u_distr-sys/u_udp_server.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT	 8080
+#define MAXLINE 1024
+
 // server code
 int main(int argc, char *argv[]) {
   int sd = UDP_Open(10000);

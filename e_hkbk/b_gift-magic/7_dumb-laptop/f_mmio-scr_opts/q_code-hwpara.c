7e63d58ee2ebfa379fafa428f2fa77494acd74f8
Andrew Green <andrewgreen031991@gmail.com>
2024-03-27 17:03:52 -0700

    x

---
+++ e_hkbk/b_gift-magic/7_dumb-laptop/f_mmio-scr_opts/q_code-hwpara.c
@@ -0,0 +1,17 @@
+
+// "... the first thing I'm going to do is encode all of these VGA timing signals as hash-define's."
+
+#define RES 1024
+#define HSYNC 0x00100000
+#define VSYNC 0x00200000
+
+#define BLUE
+#define GREEN
+#define RED
+
+#define HSYNCSTART
+#define HSYNCEND
+#define VSYNCSTART
+#define VSYNCEND
+
+//

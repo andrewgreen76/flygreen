5f9f8d74805a80e6257d4b9933b2bf196d51d9e6
Andrew Green <andrewgreen031991@gmail.com>
2024-05-06 11:21:59 -0700

    x

--- e_hkbk/b_gift-magic/u_net_java+/jjj_95_js-web-more/array_undef.js
+++ e_hkbk/b_gift-magic/u_net_java+/jjj_95_js-web-more/array_undef.js
@@ -1,7 +1,7 @@
 
 let array = [1, 2, 3];
-//array[6] = 9;
-//console.log(array[5]);
+array[6] = 9;
+console.log(array[5]);
 console.log(array.length);
 
 // Thanks, means the undefined values also count.

744676781ff2732330c690d33efa270072a4ab94
Andrew Green <andrewgreen031991@gmail.com>
2024-05-30 04:54:25 -0700

    x

---
+++ e_hkbk/c_history/0.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main(){
+  int a;
+  printf("Random number : %p\n" , (void*) &a );
+}
---
+++ e_hkbk/c_history/1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+exec_src() {
+
+    if [[ $1 =~ \.([^./]+)$ ]]; then
+	ext="${BASH_REMATCH[1]}"
+	if [[ $ext == "c" ]]; then
+	    gcc $1 -o zun.out
+	    ./zun.out
+	    rm zun.out
+	elif [[ $ext == "cpp" ]]; then
+	    g++ $1 -o zun.out
+	    ./zun.out
+	    rm zun.out
+	elif [[ $ext == "py" ]]; then
+	    python3 $1
+	elif [[ $ext == "sh" ]]; then
+	    bash $1
+	else
+	    echo "Per x (): Unknown extension"
+	fi
+    fi
+
+}
+
+for i in {1..10}; do
+    exec_src $1
+done

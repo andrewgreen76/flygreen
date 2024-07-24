3ad41f2ecfefab5a267e0f5ef902724d0c3a814e
Andrew Green <andrewgreen031991@gmail.com>
2024-07-24 13:16:56 -0700

    x

--- e_hkbk/b_gift-magic/5_game-display/soul.c
+++ e_hkbk/b_gift-magic/5_game-display/soul.c
@@ -13,8 +13,8 @@ int main(int argc, char *argv[]) {
 
     // Create a new window
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Hello, GTK!");
-    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+    gtk_window_set_title(GTK_WINDOW(window), "The Soul of the Nation");
+    gtk_window_set_default_size(GTK_WINDOW(window), 600, 400);
     g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
 
     // Display all widgets

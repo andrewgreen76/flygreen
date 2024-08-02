06a7b26a9e53590270e19c694f78d89cfa191901
Andrew Green <andrewgreen031991@gmail.com>
2024-08-02 13:31:43 -0700

    x

--- e_hkbk/b_gift-magic/9_asm-x86/3_disp-num.asm
+++ e_hkbk/b_gift-magic/9_asm-x86/3_disp-num.asm
@@ -2,6 +2,7 @@
 	
 section .data
     number db "645", 0xA   ; number as string + line feed 
+
 	
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 section .text
@@ -10,16 +11,19 @@ section .text
 
 	
 _start:
-    ; Prepare for sys_write
-    mov eax, 4            ; sys_write system call number
-    mov ebx, 1            ; File descriptor (stdout)
-    mov ecx, number       ; Buffer containing the string
-    mov edx, 4            ; Length of the string to write (including newline)
+    ; Prepare for sys_write : a, b, c, d 
+    mov eax, 4            ; system call number 	: sys_write 
+    mov ebx, 1            ; file descriptor    	: stdout 
+    mov ecx, number       ; buffer 		: string 
+    mov edx, 4            ; strlen + delim	: [know this ahead of time] 
 
     ; Execute sys_write
     int 0x80              ; Call kernel
 
+	
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     ; Exit the program
     mov eax, 1            ; sys_exit system call number
     xor ebx, ebx          ; Exit status (0 for success)
     int 0x80              ; Call kernel
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
---
+++ e_hkbk/b_gift-magic/9_asm-x86/4_disp-phr.asm
@@ -0,0 +1,29 @@
+	;; 
+	
+section .data
+    number db "Hello, world!", 0xA   ; number as string + line feed 
+
+	
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+section .text
+    global _start         ; Entry point for linking
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	
+_start:
+    ; Prepare for sys_write : a, b, c, d 
+    mov eax, 4            ; system call number 	: sys_write 
+    mov ebx, 1            ; file descriptor    	: stdout 
+    mov ecx, number       ; buffer 		: string 
+    mov edx, 4            ; strlen + delim	: [know this ahead of time] 
+
+    ; Execute sys_write
+    int 0x80              ; Call kernel
+
+	
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ; Exit the program
+    mov eax, 1            ; sys_exit system call number
+    xor ebx, ebx          ; Exit status (0 for success)
+    int 0x80              ; Call kernel
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

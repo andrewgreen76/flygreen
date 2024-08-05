81f213dba7e4560420688084bbf4c7438611d438
Andrew Green <andrewgreen031991@gmail.com>
2024-08-05 07:13:55 -0700

    x

--- e_hkbk/b_gift-magic/b_asm-x86/7_basic_entry-exit.asm
+++ e_hkbk/b_gift-magic/b_asm-x86/7_basic_entry-exit.asm
@@ -15,6 +15,7 @@ _start:
     ; Exit the program
     mov eax, 1            ; sys_exit system call number
     xor ebx, ebx          ; Exit status (0 for success)
+			  ; not 'mov ebx, 0' for optimization reasons 
     int 0x80              ; Call kernel
 
 	;; 

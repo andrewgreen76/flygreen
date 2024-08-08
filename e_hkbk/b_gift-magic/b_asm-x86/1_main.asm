4d85d12ba403e16cfcbbd6395939b9d7532331d3
Andrew Green <andrewgreen031991@gmail.com>
2024-08-08 13:41:02 -0700

    x

--- e_hkbk/b_gift-magic/b_asm-x86/1_main.asm
+++ e_hkbk/b_gift-magic/b_asm-x86/1_main.asm
@@ -1,28 +1,19 @@
-
+	
+section .bss
+	
 section .data
-	;; outbuf [0] [1]   ... 
-	out_addr db 0 , 10 	; reserving bytes for the output buffer. 
+	bytcdbuf_addr db "Enter a digit: " , 10 
+	bytecount equ $ - bytcdbuf_addr
 	
 section .text
-	global _start
+	global _start    	
 _start:
-	;; DIRECT , IN-FILE NUMERICAL INPUT 
-	mov eax, 5		
-	;; ADD ; result will be in the accumulator. 
-	add eax, 2		
-	;; CONVERT NUM.DIGIT TO CHAR 
-	add eax , 48		; add 48 to convert 7 (bell) to ASCII code 55 ('7')
-				;; more popular way : add al, '0' 
-	;; MOVE RESULT TO OUTPUT BUFFER
-	mov byte [out_addr] , al  ; "7" -> out[0]
-				  ;  \n -> out[1] 
-	;; PRINT 
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, out_addr	; start with 7 ...
-	mov edx, 2		; ... parse forth and finish with line feed 
-	int 0x80		; Call the kernel to execute. 
-    ; Exit the program
-	mov eax, 1      ; sys_exit system call number
-	xor ebx, ebx    ; Exit status (0 for success)
-	int 0x80        ; Call kernel 
+	mov eax, 4             
+	mov ebx, 1             
+	mov ecx, bytcdbuf_addr
+	mov edx, bytecount  
+	int 0x80	
+    ; Exit the program : 
+	mov eax, 1       
+	xor ebx, ebx     
+	int 0x80         
--- e_hkbk/b_gift-magic/b_asm-x86/s_mov-ex-abcd.asm
+++ e_hkbk/b_gift-magic/b_asm-x86/s_mov-ex-abcd.asm
@@ -1,5 +1,15 @@
 
-	mov eax , 0 		;; stdin , sys_read 
+	;; System calls : 
+	mov eax , 0 		;; invalid ; undefined behavior 
+	mov eax , 1 		;; sys_exit 
+	mov eax , 2 		;; sys_fork  
+	mov eax , 3 		;; sys_read  
+	mov eax , 4 		;; sys_write 
+	mov eax , 5 		;; sys_open 
+	mov eax , 6 		;; sys_close 
+	mov eax , 7 		;; sys_waitpid 
+	mov eax , 8 		;; sys_mount 
+	mov eax , 9 		;; sys_write 
 	
 	int 0x80
 

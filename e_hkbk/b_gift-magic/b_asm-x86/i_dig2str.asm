ec254a410c40c7ae0388176d371d5b69ac24c18f
Andrew Green <andrewgreen031991@gmail.com>
2024-08-08 01:58:43 -0700

    x

--- e_hkbk/b_gift-magic/b_asm-x86/i_dig2str.asm
+++ e_hkbk/b_gift-magic/b_asm-x86/i_dig2str.asm
@@ -1,7 +1,7 @@
 
 section .data
 	;; outbuf 0   outbuf 1   ... 
-	outbuf db 0 , 0
+	outbuf db 0 , 10 	; a method of reserving bytes for a string buffer. 
 
 
 	
@@ -10,12 +10,11 @@ section .text
 _start:
 	;; CONVERT NUM TO CHAR 
 	xor eax, eax 		; clear entire acc. 
-	mov al, 5		; 5 -> an acc byte 
+	mov al, 5		; 5 -> an acc byte
+	add al, 2
 	add al , '0'		; add 48 to digit to convert it to ASCII char (5+48=53) 
-	mov ah, 10		; newline -> different acc byte  
 	;; MOVE TO OUTPUT BUFFER
 	mov byte [outbuf] , al	 ;  5 -> out[0]
-	mov byte [outbuf+1] , ah ; \n -> out[1]
 	;; PRINT 
 	mov eax, 4
 	mov ebx, 1

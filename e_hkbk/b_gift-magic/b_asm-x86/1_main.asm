fd83dd959c6397d21e6717ebcf95ca82527c0890
Andrew Green <andrewgreen031991@gmail.com>
2024-08-08 18:31:22 -0700

    x

--- e_hkbk/b_gift-magic/b_asm-x86/1_main.asm
+++ e_hkbk/b_gift-magic/b_asm-x86/1_main.asm
@@ -1,15 +1,24 @@
-section	.text
-   global _start        ;must be declared for linker (ld)
+
+section .bss 
 	
-_start:                 ;tell linker entry point
-   mov	edx,9		;message length
-   mov	ecx, stars	;message to write
-   mov	ebx,1		;file descriptor (stdout)
-   mov	eax,4		;system call number (sys_write)
-   int	0x80		;call kernel
-
-   mov	eax,1		;system call number (sys_exit)
-   int	0x80		;call kernel
-
 section	.data
-stars   times 9 db '*'
+buflen 	equ 5
+stars  	times buflen - 1 db '*'
+	db 10
+	;; alternatively :
+	;; buffer db msglen dup('*')
+	
+section	.text
+global _start 
+	
+_start:                 	
+	mov	edx, buflen	; buffer length
+	mov	ecx, stars	; address of stream buffer 
+	mov	ebx, 1		; file descriptor (stdout)
+	mov	eax, 4		; system call number (sys_write)
+	int	0x80		; call kernel
+
+	mov	eax, 1		; system call number (sys_exit)
+	int	0x80		; call kernel
+
+
---
+++ e_hkbk/b_gift-magic/b_asm-x86/5_do-nothing.asm
@@ -0,0 +1,30 @@
+
+	;; A basic assembly program that does nothing.
+	;; This is just a breakdown of the entry and exit points. 
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+;;;				START 				  ;;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; DATA SEGMENT usually goes here. 
+;; We'll just start off with no data. This is just code to get in and out. 
+	
+; CODE SEGMENT
+section .text
+	
+global _start         ; Entry point for linking
+	
+_start:
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+;;;			  FINISH BY EXITING		  	  ;;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
+    ; Exit the program
+	mov eax, 1	; sys_exit system call number
+	xor ebx, ebx    ; Returns exit status 0 for success (in case if a program like the kernel asks) 
+			; Not a necessity for our purposes. 
+			; Not 'mov ebx, 0' for optimization reasons. 
+	int 0x80        ; Call kernel
+
+	;; 

7a62f956a135d38aec67796aa81d0c802dff0746
Andrew Green <andrewgreen031991@gmail.com>
2024-08-11 17:13:22 -0700

    x

--- e_hkbk/b_gift-magic/a_hex/v_key-cmds.sh
+++ e_hkbk/b_gift-magic/a_hex/v_key-cmds.sh
@@ -4,7 +4,10 @@ stat -c%s [target_file]    # byte count (incl. \0)
 file [minimal_elf.bin]     # file properties
     # minimal_elf.bin: ELF 64-bit LSB executable, x86-64, version 1 (SYSV), statically linked, stripped
 
-gdb [minimal_elf.bin]
+gdb [minimal_elf.bin]      # loading the bin exec    - ~ (gdb) file [prog.bin]
+                           # setting the entry point - ~ (gdb) set architecture i386
+                                                     # ~ (gdb) set $pc = 0x08048000
+                           # running the executable  - ~ (gdb) run
 
 readelf -h [elf_prog]
 

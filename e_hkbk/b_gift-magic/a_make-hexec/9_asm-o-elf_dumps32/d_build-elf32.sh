ef6606afbf690c119863b0a630440874533fac95
Andrew Green <andrewgreen031991@gmail.com>
2024-08-13 22:27:55 -0700

    x

--- e_hkbk/b_gift-magic/a_make-hexec/9_asm-o-elf_dumps32/d_build-elf32.sh
+++ e_hkbk/b_gift-magic/a_make-hexec/9_asm-o-elf_dumps32/d_build-elf32.sh
@@ -1,7 +1,28 @@
-o_fname=g_o32.o
+o_filename=g_o32.o
+o_profname=j_asmr-o32-prof.txt
+o_dumpname=m_asmr-o32-dump.txt
 
-nasm -f elf32 -o $o_fname 5_gen.asm
-ld -m elf_i386 -o t_test32.elf $o_fname
+e_filename=p_test32.elf
+e_profname=t_ld-e32-prof.txt
+e_dumpname=w_ld-e32-dump.txt
+
+#========= .o reflects OS/bit-width =========
+
+nasm -f elf32 -o $o_filename 5_gen.asm
+
+odjdump -f $o_filename > $o_profname
+readelf -h $o_filename >> $o_profname
+hexdump -C $o_filename > $o_dumpname
+
+#======== .elf reflects OS/btwd+arch ========
+
+ld -m elf_i386 -o t_test32.elf $o_filename
+
+odjdump -f $e_filename > $e_profname
+readelf -h $e_filename >> $e_profname
+hexdump -C $e_filename > $e_dumpname
+
+#############################################
+
 ./t_test32.elf
-hexdump -C $o_fname > j_asmr-o32dump.txt
-hexdump -C t_test32.elf > w_ld-e32dump.txt
+

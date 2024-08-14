3465ffa8f0619eb721eaaad7d75764828a254096
Andrew Green <andrewgreen031991@gmail.com>
2024-08-13 21:53:55 -0700

    x

--- e_hkbk/b_gift-magic/a_make-hexec/9_asm-o-elf_dumps32/d_build-elf32.sh
+++ e_hkbk/b_gift-magic/a_make-hexec/9_asm-o-elf_dumps32/d_build-elf32.sh
@@ -3,5 +3,5 @@ o_fname=g_o32.o
 nasm -f elf32 -o $o_fname 5_gen.asm
 ld -m elf_i386 -o t_test32.elf $o_fname
 ./t_test32.elf
-hexdump -C $o_fname > m_asmr-o32dump.txt
+hexdump -C $o_fname > j_asmr-o32dump.txt
 hexdump -C t_test32.elf > w_ld-e32dump.txt

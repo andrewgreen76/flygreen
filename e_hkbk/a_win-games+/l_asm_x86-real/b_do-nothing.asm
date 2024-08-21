22542826c89ee7ce130177ef1160c4b453631715
Andrew Green <andrewgreen031991@gmail.com>
2024-08-21 01:31:28 -0700

    x

---
+++ e_hkbk/a_win-games+/l_asm_x86-real/b_do-nothing.asm
@@ -0,0 +1,40 @@
+
+	;; The code here written in this fashion is meant for systems
+	;; that natively only support the real mode (16-bit systems).
+	;; 
+	;; This is to be assembled by the emu8086 application on Windows. 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Pre- main entry directives : 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	org 100h    	; The code below (here it's just one instruction) will be
+			; placed at this address - the OFFSET from the start of
+			; the memory layout of the process. 
+			;; 
+			;; This is NOT an absolute address. 
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; Memory segment : 
+;
+;  @   0h : Program Segment Prefix (PSP) in a DOS .COM file
+;            . environment variables 
+;            . other info 
+;
+;  @ 100h : the OFFSET within the process layout 
+;           where the code segment is to be situated 
+; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	;; We get in ...
+	
+	ret         	; ... and we get out.
+			; This is the exit point (from the subroutine). 
+
+/*********************************************************************************
+
+DOS
+
+**********************************************************************************
+*/

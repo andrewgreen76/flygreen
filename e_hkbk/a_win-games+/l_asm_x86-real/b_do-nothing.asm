7d26710987c26b789ec25fa054c961baa0d44c16
Andrew Green <andrewgreen031991@gmail.com>
2024-08-22 02:46:33 -0700

    x

--- e_hkbk/a_win-games+/l_asm_x86-real/b_do-nothing.asm
+++ e_hkbk/a_win-games+/l_asm_x86-real/b_do-nothing.asm
@@ -1,34 +1,5 @@
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Pre- main entry directives : 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-	org 100h    	; The code below (here it's just one instruction) will be
-			; placed at this address - the OFFSET from the start of
-			; the memory layout of the process. 
-			;; 
-			;; This is NOT an absolute address. 
-	
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
-; Memory segment : 
-;
-;  @   0h : Program Segment Prefix (PSP) in a DOS .COM file
-;            . environment variables 
-;            . other info 
-;
-;  @ 100h : the OFFSET within the process layout 
-;           where the code segment is to be situated 
-; 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-	;; DOS kernel gives this program the CONTROL OVER THE CPU. 
-	;; We start execution of the MAIN CODE of the program.
-	;; We get into the main SUBROUTINE ... 
-	
-	ret         	; ... and we get out.
-			; (This is the exit point from the main subroutine).
-
-	;; Upon termination of this process we give control over the CPU back to the DOS kernel. 
+	org 100h    	
+	ret
 
 	;; 

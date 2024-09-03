c7ad179852f4ef3854bddd0925c631e4873dae67
Andrew Green <andrewgreen031991@gmail.com>
2024-09-02 19:29:19 -0700

    x

---
+++ e_hkbk/b_gift-magic/c_asm_x86-real/p_subrout/k_own-int.asm
@@ -0,0 +1,33 @@
+
+;############################################################
+	;; THIS CODE IS FOR REAL-MODE ASSEMBLY ONLY !
+;############################################################
+
+	org 100h
+	
+	push ds
+	;;;;;;;;;;;;;;;;;;;;;;;
+
+	mov ax, 0
+	mov ds, ax
+	mov [0x00] , handle_int0
+	mov [0x02] , cs
+
+	int 0x00
+
+	;;;;;;;;;;;;;;;;;;;;;;;
+	pop ds
+
+	ret
+           
+;############################################################
+
+; This is just simple of way of isolating a routine from
+; the program's main flow - by placing the routine after
+; the final `ret`, or after the main code.
+ 
+handle_int0:
+	mov ah , 0eh
+	mov al , 'A'
+	int 0x10      ; out interrupt will depend on another interrupt
+	iret          ; return from int	

66348db803344786609936d83133b4bcfe8507d0
Andrew Green <andrewgreen031991@gmail.com>
2024-09-07 10:50:43 -0700

    x

--- e_hkbk/b_gift-magic/e_asm86-prot_win/5_hello.asm
+++ e_hkbk/b_gift-magic/e_asm86-prot_win/5_hello.asm
@@ -1,10 +1,14 @@
 
-	global _main
-	extern _printf
+; _underscore is used to differentiate custom labels for the project
+;; from the the similar , standard labels expected by the system ,
+;; such as `main` and `printf`. 
 
-	section .text
-_main:
-	push message
+	global _main		; _main is visible to the outside (in project development) 
+	extern _printf		; _printf is visible to this program (from elsewhere in this project) 
+	
+	section .text		; meaning that instructions are below 
+_main:				
+	push message		
 	call _printf
 	add esp , 4
 	ret

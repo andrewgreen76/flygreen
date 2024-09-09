fd72b6b328d2b97b358f2ce941d7e8d546d632d8
Andrew Green <andrewgreen031991@gmail.com>
2024-09-09 15:55:47 -0700

    x

--- e_hkbk/b_gift-magic/e_asm86-prot_win/v_push-stkfrm.asm
+++ e_hkbk/b_gift-magic/e_asm86-prot_win/v_push-stkfrm.asm
@@ -7,6 +7,21 @@ Creating a stack frame organizes and manages local variables, function parameter
 "
 */
 	
-	;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Create a stack frame ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	push ebp            ; Save the old base pointer onto the stack
+	mov ebp, esp        
+	;; The expectation is that BP would be manipulated (for changing things around
+	;;   the scope/stack)  but SP would remain the same. 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+	
+	; ... (code to use the new stack frame goes here)
+
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Restore the old stack frame ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	mov esp, ebp        
+	pop ebp             
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	
 	;; 

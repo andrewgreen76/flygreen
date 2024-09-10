bde2c0fffa67bed8558fab3ff7c40f8caa6b0ea6
Andrew Green <andrewgreen031991@gmail.com>
2024-09-09 18:52:59 -0700

    x

---
+++ e_hkbk/b_gift-magic/e_asm86-prot_win/s_push-stkfrm.asm
@@ -0,0 +1,35 @@
+
+;;; Scope vars are pushed top down.
+;;; Func args are pushed right to left + ret_addr. 
+
+/*########################################################################################*/
+/*########################################################################################*/
+/*########################################################################################*/
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Create a stack frame ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	push ebp            ; Save the old base pointer onto the stack
+	mov ebp, esp        
+	;; The expectation is that BP would be manipulated (for changing things around
+	;;   the scope/stack)  but SP would remain the same. 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	
+	; ... (code to use the new stack frame goes here)
+
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Restore the old stack frame ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	mov esp, ebp        
+	pop ebp             
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+/*########################################################################################*/
+/*########################################################################################*/
+/*########################################################################################*/
+	
+/*
+"
+Creating a stack frame organizes and manages local variables, function parameters, 
+  and return addresses, ensuring that function calls and returns are handled correctly 
+  and that each function's execution context is preserved.
+"
+*/

407f71a479a53586a976bab7cf3bc30dc881e27e
Andrew Green <andrewgreen031991@gmail.com>
2024-09-11 17:54:08 -0700

    x

--- e_hkbk/b_gift-magic/e_asm86-prot_win/p_push-stkfrm.asm
+++ e_hkbk/b_gift-magic/e_asm86-prot_win/p_push-stkfrm.asm
@@ -1,48 +1,42 @@
 
-;;; Every stack frame is a call/procedure/scope.
-;;; New srout call
-;;;   => new stack frame
-;;;   => more pushing/popping 
-;;;   => another BP
-;;;   => another frame of reference for monitoring/looking up/changing values within a scope. 
+;;; Every stack frame helps keep track of the currently active subroutine (program flow). 
+;;; Every stack frame means a call for a deeper scope.
+
+;;; Base Pointer : 
+;;;  . a frame of reference for monitoring/looking up/changing values within a scope. 
 	
 ;;; Stack frame structure (build) :
-;;;  . N pushes params for N+1 last to first  <->  push in-caller post-call (ret) IP
-;;;  . N+1 new srout loc vars pushed top down 
+;;;  .  caller-to-callee params , last to first
+;;; <-> caller's CS:EIP (for ret) 
+;;;  .  callee's loc vars , pushed top down 
 
-;;; Return / tear-down - gives back ctrl to prev prcdre : 
-;;;  . pop off local vars of N+1 
-;;;  . pop ret_addr off to IP (N)  <->  prcdre N pops off params of N+1 
+;;; Return / tear-down - gives back ctrl to prev procedure : 
+;;;  . pop off callee's vars 
+;;;  . pop ret_addr off to caller's CS:EIP
+;;; <->  prcdre N pops off params of N+1 
 	
 /*########################################################################################*/
 /*#################################### PROLOGUE CODE #####################################*/
-/*################################ Create a stack frame ##################################*/
+/*############################## Create a srout stack frame ##############################*/
 /*########################################################################################*/
 	push ebp            ; Save the old base pointer onto the stack
-	mov ebp, esp        
-	;; The expectation is that BP would be manipulated (for changing things around
-	;;   the scope/stack) but SP would remain the same. 
+	mov ebp, esp
+	sub esp , (frame_size)
+	;; Then the compiler allocates and initializes the pieces of the stack frame , incl.
+	;; the caller's CS:EIP. 
+	
 /*########################################################################################*/
-/*########################################################################################*/
+/*###################### Code to use the new stack frame goes here #######################*/
 /*########################################################################################*/
 	
-	; ... (code to use the new stack frame goes here)
-	;; Let's start with calling a srout that does nothing. 
+	;; Let's start studying stack frames with calling an arg-less srout that does nothing.
+	;; So it's just the caller's CS:EIP. 
 	
 /*########################################################################################*/
 /*#################################### EPILOGUE CODE #####################################*/
-/*############################# Restore the old stack frame ##############################*/
+/*################ Tear down this stack frame , go back to the older one ##################*/
 /*########################################################################################*/
+
+	;; First , free the stack frame. Then do the rest : 
 	mov esp, ebp        
 	pop ebp             
-/*########################################################################################*/
-/*########################################################################################*/
-/*########################################################################################*/
-	
-/*
-"
-Creating a stack frame organizes and manages local variables, function parameters, 
-  and return addresses, ensuring that function calls and returns are handled correctly 
-  and that each function's execution context is preserved.
-"
-*/

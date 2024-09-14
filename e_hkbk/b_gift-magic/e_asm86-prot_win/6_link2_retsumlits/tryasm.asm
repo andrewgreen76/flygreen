dc06cd2fdfa5ed25509e06696623d30e65770ffc
Andrew Green <andrewgreen031991@gmail.com>
2024-09-14 15:25:59 -0700

    x

--- e_hkbk/b_gift-magic/e_asm86-prot_win/6_link2_retsumlits/tryasm.asm
+++ e_hkbk/b_gift-magic/e_asm86-prot_win/6_link2_retsumlits/tryasm.asm
@@ -4,15 +4,15 @@
 	section .text 	
 _my_asm:
 
-	push ebp		; SP is moved automatically 
+	push ebp	; SP is moved automatically 
 	mov ebp , esp   ; BP -> SP -> 
 
 	;;  Creating a local scope : 
 	sub esp, 8    	; create a gap for two local ints ( sizeof(int) = 4 bytes ) 
 
 	mov dword[ebp-4], 30    ; byte = 8 bits @@@ word = 2 bytes = 16 bits @@@ dword = 4 bytes = 32 bits
-							; go to EBP-4, populate double-word (4 bytes) , 
-							; initialize that memory piece with num 30. 
+				; go to EBP-4, populate double-word (4 bytes) , 
+				; initialize that memory piece with num 30. 
 	mov dword[ebp-8], 80
 	mov eax, dword[ebp-4]
 	add eax, dword[ebp-8]
@@ -20,14 +20,22 @@ _my_asm:
 	;; Closing the local scope : 
 	add esp, 8  ; wind down , forget all vars 
 
-	pop ebp		; SP is moved automatically 
+	pop ebp    ; SP is moved automatically 
 	ret
 
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; MEMORY ALIGNMENT 
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
 	; Notice the presence of '4' in the instructions. This is called memory alignment: In 32-bit 
 	;   systems all offsets should be multiples of 4 , even for data that could easily fit into 
 	;   3, 2, or even 1 byte. Otherwise , the integrity of the stack frame is violated and we have 
 	;   corrupted memory on our hands. Therefore , using 'dword[]' instead of 'word[]' and 'byte[]' 
 	;   is also preferrable. 
+
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; OBSERVING PARAMETERS AND LOCAL SCOPE DATA  
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	
 	; Also , notice that we do [ebp + offset] to access arguments and we do [ebp - offset] to access 
 	;   local variables/structures/objects. Don't forget to account for the caller's return address 

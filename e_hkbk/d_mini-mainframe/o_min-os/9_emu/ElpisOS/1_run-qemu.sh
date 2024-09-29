f7cc077d81c87239f2fbff14c628d6bb966115d7
Andrew Green <andrewgreen031991@gmail.com>
2024-09-28 17:07:11 -0700

    x

---
+++ e_hkbk/d_mini-mainframe/o_min-os/9_emu/ElpisOS/1_run-qemu.sh
@@ -0,0 +1,8 @@
+
+# $ qemu-system-x86_64    # Runs QEMU without anything to go on.
+
+# $ qemu-system-x86_64 -hda [urboot.bin]    # Runs QEMU off of the linked raw binary
+                                        # May involve some stripping or conversion (flattening or binary dumping) 
+
+nasm -f bin ./boot.asm -o ./boot.bin
+qemu-system-x86_64 -hda ./boot.bin
--- e_hkbk/d_mini-mainframe/o_min-os/9_emu/ElpisOS/boot.asm
+++ e_hkbk/d_mini-mainframe/o_min-os/9_emu/ElpisOS/boot.asm
@@ -3,11 +3,26 @@
 	BITS 16			
 
 start:
-	mov ah , 0eh
-	mov al , 'A'
-	int 0x10
-
+	mov si , msg
+	call print 
 	jmp $
+
+print:				; mov bx , 0
+.loop:	
+	lodsb
+	cmp al , 10
+	je .done
+	call printch
+	jmp .loop
+.done:	
+	ret
+	
+printch:	
+	mov ah , 0eh
+	int 0x10
+	ret
+
+msg:	db 'Hello, World!', 10
 
 	times 510-($-$$) db 0	
 	dw 0xAA55

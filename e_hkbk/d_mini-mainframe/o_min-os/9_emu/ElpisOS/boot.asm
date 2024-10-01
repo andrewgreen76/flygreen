623c637cf219b51309783bf02252a747cebccdaf
Andrew Green <andrewgreen031991@gmail.com>
2024-09-30 19:42:29 -0700

    x

--- e_hkbk/d_mini-mainframe/o_min-os/9_emu/ElpisOS/boot.asm
+++ e_hkbk/d_mini-mainframe/o_min-os/9_emu/ElpisOS/boot.asm
@@ -4,31 +4,20 @@
 				; BY ITSELF !
 
 start:
-	cli			; clear the int flag in FLAGS (disable interrupts) 
-	/************************************************************************/
-	/* This chunk here is critical to system operability and must not 
-	   be interrupted under any circumstances by a hardware interrupt. 
-	   If it is interrupted , consistency may be violated and wrong values may 
-	   get written to the registers. */
+	cli			; clear the int flag in FLAGS (disable interrupts)
+	;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	mov ax , 0x07c0	 	; This method is an alternative to ORG 0x7C00. 
 	
 	mov ds , ax 		; DS:SI starting address calibrated to 0x7c00. Source buffer offsets are taken care of. 
 	mov es , ax 		; ES:DI starting address calibrated to 0x7c00. Destination buffer offsets are taken care of.
-				; Data is part of our source code within the same segment @ 0x7C00. 
-	
-	/* Q : ... in other words, DS and ES must hold the same address as CS because 
-	       they are essentially "the same segment" in real mode?
+				; Data is part of our source code within the same segment @ 0x7C00.
 
-	   A : "... IN REAL MODE, having DS, ES, and CS hold the same value ensures that 
-	        all segment registers point to the same memory area, allowing consistent 
-	        access to the same code and data within that segment."  */
-
-	/* "... IN PROTECTED MODE, CS, DS, and ES can hold different values as they represent 
-	    distinct segment selectors that allow the processor to manage separate segments 
-	    of memory for code, data, and additional purposes within the same process." */
-	/************************************************************************/
-	
+	mov ax , 0x00
+	mov ss , ax
+	mov sp , 0x7c00 	; Keep in mind : this is a pointer register , not a segment register.
+	;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;	
 	sti			; enable interrupts
+	
 	mov si , msg
 	call print_msg 
 	jmp $			; "halting" point

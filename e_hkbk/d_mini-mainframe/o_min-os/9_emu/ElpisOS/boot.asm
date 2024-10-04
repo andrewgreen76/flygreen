7801463ec3ae05b9b77f4294f1aa93ced836db74
Andrew Green <andrewgreen031991@gmail.com>
2024-10-03 18:56:55 -0700

    x

--- e_hkbk/d_mini-mainframe/o_min-os/9_emu/ElpisOS/boot.asm
+++ e_hkbk/d_mini-mainframe/o_min-os/9_emu/ElpisOS/boot.asm
@@ -5,17 +5,17 @@
 _start:
 	jmp short ld_btld
 	nop 			 
-	times 33 db 0 	; Bullshitted BPB to 36 bytes from 0x0. 
+	times 33 db 0 	; Faked all {0x03 -> 0x35} data bytes for BPB / 512. 
 
 ld_btld:
-	jmp 0x07c0:step2 ; Manual load of CS:IP. 
+	jmp 0x07c0:find_n_ld ; Manual load of CS:IP. 
 
 hndl_int0:		; an ISR : prints 'A'  
 	mov ah , 0eh
 	mov al , 'A'
 	mov bx , 0
 	int 0x10
-	iret 		; end of int subrout
+	iret 		; end of ISR 
 
 hndl_int1:
 	mov ah , 0eh
@@ -24,7 +24,7 @@ hndl_int1:
 	int 0x10
 	iret
 	
-step2:	
+find_n_ld:	
 	cli			
 	mov ax , 0x07c0	 	 
 	mov ds , ax 	; DS = 0x7C00
@@ -33,7 +33,7 @@ step2:
 	mov ss , ax	; SS:0 is RAM:0
 	mov sp , 0x7c00 	
 	sti			
-	;;; PROGRAMMING LOGIC ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;;; MAPPING ADDRESSES OF CUSTOM INTS TO MAKE IVT ;;;;;;;;;;;;;
 				   ; 0x0 = IVT , not the boot code. 
 	mov word[ss:0] , hndl_int0 ; addr_isr0 = SS = 0 = RAM:0 = int0_offset   ;   Otherwise , if [0] => [DS:0] => 0x7C00 + 0 = 0x7C00
 	mov word[ss:2] , 0x07C0    ; RAM:2 = int0_seg = CS:
@@ -41,10 +41,11 @@ step2:
 	mov word[ss:4] , hndl_int1 ; RAM:3 = int1_off = :IP
 	mov word[ss:6] , 0x07C0	   ; RAM:4 = int1_seg = CS:
 
-	;; mov ax , 0
-	;; div ax			; DIV/0 , but acts as int 0
+	;;; PROGRAMMING LOGIC ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;; mov ax , 0		; Prints 'A'.
+	;; div ax		; CPU is designed to automatically generate the `int 0` exception upon DIV/0 (no matter what). 
 	
-	int 1
+	int 1			; Prints 'V'. 
 	
 	mov si , msg 		; Print "Hello, World!"
 	call print_msg 

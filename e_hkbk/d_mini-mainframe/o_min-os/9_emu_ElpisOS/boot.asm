f70706d217c44bbee802e11ec3991578cbefd3f3
Andrew Green <andrewgreen031991@gmail.com>
2024-10-10 20:43:25 -0700

    x

--- e_hkbk/d_mini-mainframe/o_min-os/9_emu_ElpisOS/boot.asm
+++ e_hkbk/d_mini-mainframe/o_min-os/9_emu_ElpisOS/boot.asm
@@ -1,50 +1,87 @@
 
-	ORG 0			 
-	BITS 16			
-				 
+	ORG 0x7c00
+	BITS 16 
+
+CODE_SEG equ gdt_code - gdt_start    ; offset of code in GDT instructions block (0x8)
+DATA_SEG equ gdt_data - gdt_start    ; offset of data in GDT instructions block (0x10)
+	
 _start:
 	jmp short ld_btld
 	nop 			 
 	times 33 db 0 	; Faked all {0x03 -> 0x35} data bytes for BPB / 512. 
 
 ld_btld:
-	jmp 0x07c0:at_7c00 ; Manual load of CS:IP. 
+	jmp 0x0:at_7c00 ; Manual load of CS:IP. 
 	
 at_7c00:	
-	cli			
-	mov ax , 0x07c0	 	 
+	cli
+	mov ax , 0x0
 	mov ds , ax 	; DS = 0x7C00
 	mov es , ax	; ES = -//-
-	mov ax , 0x00
 	mov ss , ax	; SS:0 is RAM:0
 	mov sp , 0x7c00 	
 	sti 
-	;;; MAPPING ADDRESSES OF CUSTOM INTS TO MAKE IVT ;;;;;;;;;;;;;
-	;;; PROGRAMMING LOGIC ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	jmp $ 
-	;;; GDT ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; PROGRAMMING LOGIC ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+.ld_prot:
+	cli
+	lgdt [gdt_descr] 	; acts on size and offset - that's all it demands
+	mov eax , cr0
+	or eax , 0x1
+	mov cr0 , eax
+	jmp CODE_SEG:ld32 	; 0x8:"halt" 
+	
+;;; GDT CODE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 gdt_start: 
-gdt_null:			; null descriptor 
+gdt_null:			; null descriptor (64 bits) - represents an invalid segment 
 	dd 0x0 			; define dword (word = bits , dword = 32 bits)
 	dd 0x0 			; we've defined 2*23 = 64 bits with nulls
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	;; CS descriptor = 0x8 = offset in the table 
-gdt_code: 			; CS will have to point to this. 
-	dw 0xffff 		; ??? seg_lim : 0-15 bits ???
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	
-print_msg:
-	mov ah , 0eh
-.chk_char:			 
-	lodsb
-	cmp al , 0 
-	je .out			
-	int 0x10		 
-	jmp .chk_char		 
-.out:
-	ret
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; CS descriptor = 0x8 = offset in the table (8-byte offset in GDT instructions block)
+gdt_code: 			; CS @ here  
+	dw 0xffff 		; GDTdescr : 15-00 : seg.lim=15
+	dw 0 			; GDTdescr : 31-16 : seg.base(0-15)=0
+	db 0 			; GDTdescr : 39-32 : seg.base_ext(16-23)=0
+	db 0x9a			; GDTdescr : 47-40 : seg.access_byte (bit mask) 
+					;; P DPL S 	Type 	; DPL - Descriptor Privilege Level 
+					;;   0x9	0xA 
+					;; 1  00 1	1010
+	db 11001111b		; GDTdescr : 55-48 : seg.flags
+	db 0 			; GDTdescr : 63-56 : seg.base_ext(24-31)=0
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; otherS_descr = 0x10 = offset in the table (10-byte offset in GDT instructions block)
+gdt_data:	      		; Linked to DS , ES , FS , GS , SS 
+	dw 0xffff 		; GDTdescr : 15-00 : seg.lim=15
+	dw 0 			; GDTdescr : 31-16 : seg.base(0-15)=0
+	db 0 			; GDTdescr : 39-32 : seg.base_ext(16-23)=0
+	db 0x92			; GDTdescr : 47-40 : seg.access_byte (bit mask) 
+					;; P DPL S 	Type 	; DPL - Descriptor Privilege Level 
+					;;   0x9	0xA 
+					;; 1  00 1	1010
+	db 11001111b		; GDTdescr : 55-48 : seg.flags
+	db 0 			; GDTdescr : 63-56 : seg.base_ext(24-31)=0
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+gdt_end:
 
-	;;; 1st sector has the code below for populating hardcoded chars. ;;;;;;;;;;;;;
+gdt_descr:
+	dw gdt_end - gdt_start - 1    ; size(16b) of descriptor
+	dd gdt_start		      ; offset(32b) of descriptor    ; null + CSdescr + DSdescr will be loaded 
+
+;;; 32-BIT CODE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	[BITS 32] 		
+	
+ld32:
+	mov ax , DATA_SEG
+	mov ds , ax
+	mov es , ax
+	mov fs , ax
+	mov gs , ax
+	mov ss , ax
+	mov ebp , 0x00200000
+	mov esp , ebp 
+	jmp $
+	
+;;; 1st sector has the code below for populating hardcoded chars. ;;;;;;;;;;;;;
 
 	times 510-($-$$) db 0	
 	dw 0xAA55

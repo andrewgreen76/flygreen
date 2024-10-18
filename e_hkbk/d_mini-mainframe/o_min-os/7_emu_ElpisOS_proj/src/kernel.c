985e00132cd7670e67e1776fc58248d0f44c80a6
Andrew Green <andrewgreen031991@gmail.com>
2024-10-18 10:37:36 -0700

    x

--- e_hkbk/d_mini-mainframe/o_min-os/7_emu_ElpisOS_proj/src/kernel.c
+++ e_hkbk/d_mini-mainframe/o_min-os/7_emu_ElpisOS_proj/src/kernel.c
@@ -1,9 +1,12 @@
 #include "kernel.h"
 
-void kernel_main()
+void kernel_main()    // kernel_main - token globalized out to kernel.asm // call kernel_main ; ret ; jmp $
 {
   char * video_mem = (char *) (0xB8000);
-  video_mem[0] = 'Y';
+  // 0xB8000 is treated as a pointer to the original byte in video memory. 
+
+  // Offsets : 
+  video_mem[0] = 'Y';                       
   video_mem[1] = 2;
   video_mem[2] = 'e';
   video_mem[3] = 15;

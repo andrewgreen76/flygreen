60e545900c162976322a9c1c598e2ad9e0b70f6b
Andrew Green <andrewgreen031991@gmail.com>
2024-10-18 09:48:22 -0700

    x

--- e_hkbk/d_mini-mainframe/o_min-os/7_emu_ElpisOS_proj/src/kernel.c
+++ e_hkbk/d_mini-mainframe/o_min-os/7_emu_ElpisOS_proj/src/kernel.c
@@ -3,6 +3,10 @@
 void kernel_main()
 {
   char * video_mem = (char *) (0xB8000);
-  video_mem[0] = 'A';
+  video_mem[0] = 'Y';
   video_mem[1] = 2;
+  video_mem[2] = 'e';
+  video_mem[3] = 15;
+  video_mem[4] = 's';
+  video_mem[5] = 8;
 }

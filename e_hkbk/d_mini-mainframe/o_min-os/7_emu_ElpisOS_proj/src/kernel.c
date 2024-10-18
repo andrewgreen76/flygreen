cb7eb75a7a426d979af0385779cb98d6879db76a
Andrew Green <andrewgreen031991@gmail.com>
2024-10-18 11:09:27 -0700

    x

--- e_hkbk/d_mini-mainframe/o_min-os/7_emu_ElpisOS_proj/src/kernel.c
+++ e_hkbk/d_mini-mainframe/o_min-os/7_emu_ElpisOS_proj/src/kernel.c
@@ -1,15 +1,39 @@
 #include "kernel.h"
+#include <stdint.h>
+
+uint16_t terminal_make_char(char c , char color)
+{
+  return (color << 8) | c;
+  /*
+        00000000 00000010 << 8
+        00000010 00000000 or 00000000 01011001 
+        00000010 01011001
+   */
+}
 
 void kernel_main()    // kernel_main - token globalized out to kernel.asm // call kernel_main ; ret ; jmp $
 {
-  char * video_mem = (char *) (0xB8000);
-  // 0xB8000 is treated as a pointer to the original byte in video memory. 
-
-  // Offsets : 
+  /* 
+  char_t * video_mem = (char *) (0xB8000);
   video_mem[0] = 'Y';                       
   video_mem[1] = 2;
   video_mem[2] = 'e';
   video_mem[3] = 15;
   video_mem[4] = 's';
   video_mem[5] = 8;
+   */
+  
+  // 0xB8000 is treated as a pointer to the original byte in video memory.
+
+  /************************************************************************************/
+  
+  uint16_t * video_mem = (uint16_t *) (0xB8000);
+
+  // FRAMEBUFFER OFFSETS :
+  //
+  // The compiler will figure out the actual framebuffer offsets and their values 
+  //   based on (1) 'short' (taking up 2 bytes) and (2) little-endianness. 
+  video_mem[0] = 0x0259;    // {green , 'Y'} *** {color,char} - because of little-endianness. 
+  video_mem[1] = 0x0f65;    
+  video_mem[2] = 0x0873;
 }

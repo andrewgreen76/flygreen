807a1e299af92d8f5a14982fa23b1e8cd8f55c1e
Andrew Green <andrewgreen031991@gmail.com>
2024-10-18 19:02:38 -0700

    x

--- e_hkbk/d_mini-mainframe/o_min-os/7_emu_ElpisOS_proj/src/idt/idt.c
+++ e_hkbk/d_mini-mainframe/o_min-os/7_emu_ElpisOS_proj/src/idt/idt.c
@@ -2,26 +2,34 @@
 #include "config.h"
 #include "memory/memory.h"
 
+/*##########################################################################################*/
+
 struct idt_desc idt[NUM_INTERRUPTS];
 struct idtr_desc idtr_descriptor;    // about the table itself 
 
 extern void idt_load(struct idtr_desc * ptr);    // just to call lidt <= load idt.info_addr <= CPU needs to know about the IDT. 
                                                  // Compile-time security ? 
+/*##########################################################################################*/
 
 void idt_zero(){
   printstr("ERROR : DIV/0\n");
 }
 
+/*##########################################################################################*/
+
 // Helps set interrupts and their addresses within IDT : 
 void idt_set(int interrupt_i , void* interrupt_addr){
-  struct idt_desc * desc = & idt[interrupt_i];    // abbr. to 'desc'
-  desc->offset_lo = (uint32_t) interrupt_addr & 0x0000FFFF;
-  desc->selector = KERNEL_CODE_SELECTOR;
-  desc->zero = 0 ;
-  desc->type_attr = 0xEE;    // b/c we are working with an i386 32-bit interrupt gate ;
-                             // interrupt will be used by user processes.
-  desc->offset_hi = (uint32_t) interrupt_addr >> 16;
+  struct idt_desc * idesc = & idt[interrupt_i];    // abbr. to 'idesc'
+  
+  idesc->offset_lo = (uint32_t) interrupt_addr & 0x0000FFFF;
+  idesc->selector = KERNEL_CODE_SELECTOR;
+  idesc->zero = 0 ;
+  idesc->type_attr = 0xEE;    // b/c we are working with an i386 32-bit interrupt gate ;
+                              // interrupt will be used by user processes.
+  idesc->offset_hi = (uint32_t) interrupt_addr >> 16;
 }
+
+/*##########################################################################################*/
 
 // Initializes IDT w/ 0's : 
 void idt_init()

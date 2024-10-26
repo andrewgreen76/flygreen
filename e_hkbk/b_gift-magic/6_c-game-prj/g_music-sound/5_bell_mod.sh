0db68306c0cf5c369e02857b803aea7b38a47b92
Andrew Green <andrewgreen031991@gmail.com>
2024-10-26 09:54:38 -0700

    x

---
+++ e_hkbk/b_gift-magic/6_c-game-prj/g_music-sound/5_bell_mod.sh
@@ -0,0 +1,5 @@
+
+# Does not work. 
+
+echo -e "\033[10;100]\033[11;1000" # Set pitch and duration    # Hz , ms
+echo -e "\a"                      # Trigger the bell
---
+++ e_hkbk/b_gift-magic/6_c-game-prj/g_music-sound/7_beep.sh
@@ -0,0 +1,6 @@
+
+beep -f 1000 -l 500
+
+# stderr : "beep: Error: Could not open any device"
+
+#

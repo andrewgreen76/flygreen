2fe1da932cb8e25c531dfe93c7fa467e74dbdb02
Andrew Green <andrewgreen031991@gmail.com>
2024-11-03 23:54:39 -0800

    x

--- e_hkbk/d_mini-mainframe/r_min-os/7_emu_murmOS_proj/src/idt/idt.asm
+++ e_hkbk/d_mini-mainframe/r_min-os/7_emu_murmOS_proj/src/idt/idt.asm
@@ -3,8 +3,10 @@
 	section .asm  		
 
 	extern no_int_handle
-	extern int_21h_handle	; make the C func known to the .asm src file 
+	extern int_21h_handle	; make the C func known to the .asm src file
+	
 	global idt_load
+	
 	global no_int_atomic
 	global int_21h_atomic	; make the ASM routine global/known to the C src file 
 	
@@ -20,18 +22,18 @@ idt_load:
 	ret
 
 ;;; Weaving in and out assembly just to ensure the atomicity of the int handler : ;;;;;;;;;;
-int_21h_atomic:
-	cli 			; same for interrupts - atomic kernel op 
-	pushad 			; saves values in all gen.regs
-	call int_21h_handle
-	popad 			; restores old values in gen.regs 
-	sti
-	iret
-
 no_int_atomic:
 	cli 			; same for interrupts - atomic kernel op 
 	pushad 			; saves values in all gen.regs
 	call no_int_handle
+	popad 			; restores old values in gen.regs 
+	sti
+	iret
+	
+int_21h_atomic:
+	cli 			; same for interrupts - atomic kernel op 
+	pushad 			; saves values in all gen.regs
+	call int_21h_handle
 	popad 			; restores old values in gen.regs 
 	sti
 	iret

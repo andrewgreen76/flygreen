708412a582aab74f3cd263ef5681ea4a830a993e
Andrew Green <andrewgreen031991@gmail.com>
2024-11-03 21:25:55 -0800

    x

---
+++ e_hkbk/b_gift-magic/r_js-gui/z_electron/main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadURL('data:text/html;charset=utf-8,<html><body><h1>Blank Window</h1></body></html>');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});

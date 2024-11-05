778fffceed67d63c404b44a9d79cf14ea17f06c9
Andrew Green <andrewgreen031991@gmail.com>
2024-11-05 07:47:41 -0800

    x

--- e_hkbk/d_mini-mainframe/r_min-os/7_emurmOS_proj/src/memory/heap/heap.h
+++ e_hkbk/d_mini-mainframe/r_min-os/7_emurmOS_proj/src/memory/heap/heap.h
@@ -4,7 +4,18 @@
 #include <stdint.h>
 #include <stddef.h>
 
-#define HEAPBLK_TAKEN 0x01 
-#define HEAPBLK_FREE 0x00 
+// Bitmasks to op with heap_table_entries :
+//
+#define MORE_IN_GROUP  0b10000000
+#define FIRST_IN_GROUP 0b01000000
+// bit unassigned      0b00100000
+// bit unassigned      0b00010000
+// bit unassigned      0b00001000
+// bit unassigned      0b00000100
+// bit unassigned      0b00000010
+#define HEAPBLK_TAKEN  0b00000001 
+#define HEAPBLK_FREE   0b00000000
+
+typedef unsigned char HEAPBLK_ENTRY ;
 
 #endif

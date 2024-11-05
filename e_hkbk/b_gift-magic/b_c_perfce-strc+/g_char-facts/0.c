7015f5ce3676075f8ac9b1bb8141faddb3c235a4
Andrew Green <andrewgreen031991@gmail.com>
2024-11-05 08:30:09 -0800

    x

--- e_hkbk/b_gift-magic/b_c_perfce-strc+/g_char-facts/0.c
+++ e_hkbk/b_gift-magic/b_c_perfce-strc+/g_char-facts/0.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 
 void main(){
+  char baby = 128;
+  
   printf("Size of char : %lu\n" , sizeof(char) );
-  printf("Size of int : %lu\n" , sizeof(int) );
+  printf("Size of int : %lu\n\n" , sizeof(int) );
+  
+  printf("Size of * : %lu\n\n" , sizeof(void *) );
+
+  printf("Baby is %d years old.\n" , baby);
 }

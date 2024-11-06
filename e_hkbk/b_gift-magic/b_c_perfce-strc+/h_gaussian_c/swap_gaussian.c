3cf956c7657be5075fb70bd8c9ed5562b7642547
Andrew Green <andrewgreen031991@gmail.com>
2024-11-06 06:59:01 -0800

    x

--- e_hkbk/b_gift-magic/b_c_perfce-strc+/h_gaussian_c/swap_gaussian.c
+++ e_hkbk/b_gift-magic/b_c_perfce-strc+/h_gaussian_c/swap_gaussian.c
@@ -1,13 +1,43 @@
+#include "stdio.h"
 
-#include <stdio.h>
+//#define s 5
+const int num_vals = 5; // Keeping it a const is good practice ; we don't want this var to be altered. 
 
-#define RANGE 100
+int main()
+{
+  //int a[s] = {1,2,3,4,5};
+  /*
+    ain.c:8:3: error: variable-sized object may not be initialized
+    8 |   int a[s] = {1,2,3,4,5};
+      |   ^~~
+   */
+  int a[] = {1,2,3,4,5};
+  int m = num_vals/2;
+  printf("\n");
 
-void main(){
-  for(){
+  //swapping:
+  //
+  for(int i=0; i<m; i++)
+  {
+    int t = num_vals-1-i;
+    a[i] ^= a[t] ^= a[i] ^= a[t];
   }
   
-  printf("\n");
-  printf("\n");
-  printf("\n");
+  //printing:
+  //
+  for(int i=0; i<num_vals; i++)
+    printf("%i\n", a[i]);
+  
+  printf("\n");  
+  return 0;
 }
+
+/*
+- An array is a pointer 
+
+  int arr[6]={11,12,13,14,15,16};
+
+  : arr is a pointer that stores the address of the first element in the array
+
+- An array is contiguous blocks of memory that store a value
+ */

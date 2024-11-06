b37e5d450b9103cf54d9f34721f857cb8685d11a
Andrew Green <andrewgreen031991@gmail.com>
2024-11-06 06:18:19 -0800

    x

--- e_hkbk/d_mini-mainframe/r_min-os/7_emurmOS_proj/src/memory/heap/k_heap.c
+++ e_hkbk/d_mini-mainframe/r_min-os/7_emurmOS_proj/src/memory/heap/k_heap.c
@@ -6,8 +6,6 @@ struct heap k_heap;    // primitive inheritance
 struct heap_table k_table;
 
 void k_heap_init(){
-  unsigned int max_entries = HEAP_SIZE / HPBLOCK_SIZE;
-
   k_table.entries = (void *) 0x0;
-  k_table.total = max_entries; 
+  k_table.max_entries = HEAP_SIZE / HPBLOCK_SIZE; 
 }

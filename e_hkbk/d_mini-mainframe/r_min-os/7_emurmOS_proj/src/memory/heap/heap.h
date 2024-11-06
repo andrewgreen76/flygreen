e1d23cd00c250ac2e744a6bf728717a66f532966
Andrew Green <andrewgreen031991@gmail.com>
2024-11-06 07:20:58 -0800

    x

--- e_hkbk/d_mini-mainframe/r_min-os/7_emurmOS_proj/src/memory/heap/heap.h
+++ e_hkbk/d_mini-mainframe/r_min-os/7_emurmOS_proj/src/memory/heap/heap.h
@@ -30,8 +30,8 @@ struct heap_table {
 };
 
 struct heap {
-  struct heap_table * table;    // for using the heap_table 
-  void * start_addr;            // for using the heap_memory 
+  struct heap_table * reg_hptable; // for using the heap_table 
+  void * start_addr;               // for using the heap_memory 
 };
 
 //##############################################################
--- e_hkbk/d_mini-mainframe/r_min-os/7_emurmOS_proj/src/memory/heap/k_heap.c
+++ e_hkbk/d_mini-mainframe/r_min-os/7_emurmOS_proj/src/memory/heap/k_heap.c
@@ -3,9 +3,9 @@
 #include "config.h"
 
 struct heap k_heap;    // primitive inheritance
-struct heap_table k_table;
+struct heap_table k_hptable;
 
 void k_heap_init(){
-  k_table.entries = (void *) 0x0;
-  k_table.max_entries = HEAP_SIZE / HPBLOCK_SIZE; 
+  k_hptable.max_entries = HEAP_SIZE / HPBLOCK_SIZE; 
+  k_hptable.entries = (void *) 0x0;
 }

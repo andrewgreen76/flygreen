39a7d7d3afc227e7b15d3950b292e1c39a9b8ba6
Andrew Green <andrewgreen031991@gmail.com>
2025-01-08 22:15:32 -0800

    x

--- b_gift-magic/c_c_perfce-strc+/0.c
+++ b_gift-magic/c_c_perfce-strc+/0.c
@@ -1,21 +1,17 @@
-
 #include <stdio.h>
+#include <string.h>
 
-int main(void){
-  printf("\n");
+#define BUFSZ_TOTAL 6
+#define BUFSZ_TIGHT BUFSZ_TOTAL-1
+
+void main(){
+  printf("Total : %d\n" , BUFSZ_TOTAL);
+  printf("Tight : %d\n" , BUFSZ_TIGHT);
+  char buf[BUFSZ_TOTAL];
   
-  void * addr_of_main = (void*) main;
-  int a = 1;
-  int * b = &a;
-  printf("Addresses of functions : \n");
-  printf("addr_of_main : %p\n" , addr_of_main);
-  printf("\n");
-  
-  printf("Data addresses and the addresses of those addresses : \n");
-  printf("addr of addr_of_main : %p\n" , & addr_of_main);
-  printf("addr of addr_of_a : %p\n" , &b);
-  printf("addr_of_a : %p\n" , b);
-  
-  printf("\n");
-  return 0;
+  fgets(buf , BUFSZ_TOTAL , stdin);
+
+  if( strcmp(buf,"1")==0 ) printf("1");
+  if( strcmp(buf,"1\n")==0 ) printf("1\\n");
+  if( strcmp(buf,"1\0")==0 ) printf("1\\0");
 }

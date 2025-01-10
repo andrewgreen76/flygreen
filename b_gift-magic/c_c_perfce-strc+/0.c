cafaa12975fbb8bc90957f4758cdb410928cdc07
Andrew Green <andrewgreen031991@gmail.com>
2025-01-09 16:24:12 -0800

    x

--- b_gift-magic/c_c_perfce-strc+/0.c
+++ b_gift-magic/c_c_perfce-strc+/0.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define BUFSZ_TOTAL 6
-#define BUFSZ_TIGHT BUFSZ_TOTAL-1
-
 void main(){
-  printf("Total : %d\n" , BUFSZ_TOTAL);
-  printf("Tight : %d\n" , BUFSZ_TIGHT);
-  char buf[BUFSZ_TOTAL];
-  
-  fgets(buf , BUFSZ_TOTAL , stdin);
-
-  if( strcmp(buf,"1")==0 ) printf("1");
-  if( strcmp(buf,"1\0")==0 ) printf("1\\0"); 
-  if( strcmp(buf,"1\n")==0 ) printf("1\\n"); // This is the one upon 'Enter' press. 
+  int a=1;
+  for( ; a<0 ; --a) printf("Post-Decremented.\n");
 }

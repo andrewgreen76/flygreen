47630638c2744785827a80deb01b34001441696b
Andrew Green <andrewgreen031991@gmail.com>
2025-02-25 23:11:27 -0800

    x

--- a_win-games+/l_c-mchlang/x_games/4_snake/3_snake.sh
+++ a_win-games+/l_c-mchlang/x_games/4_snake/3_snake.sh
@@ -1,11 +1,13 @@
+SNAKE_SRC="4_snake_dumb_crude.c"
+
 rm a.out
-gcc 4_snake_dumb_crude.c -lm
+gcc $SNAKE_SRC -lm
 
 if [ -e ./a.out ]; then
     read -n 1 -s -r -p "Press any key to continue..."
     ./a.out
     ls -1
-    emacs -nw 4_snake_dumb_crude.c
+    emacs -nw $SNAKE_SRC
 else
     echo "Executable \"\"\"./a.out\"\"\" does NOT exist."
 fi
--- a_win-games+/l_c-mchlang/x_games/7_pong/0_pong.sh
+++ a_win-games+/l_c-mchlang/x_games/7_pong/0_pong.sh
@@ -1,11 +1,13 @@
+PONG_SRC="pong.c"
+
 rm a.out
-gcc 0_pong.c -lm
+gcc $PONG_SRC -lm
 
 if [ -e ./a.out ]; then
     read -n 1 -s -r -p "Press any key to continue..."
     ./a.out
     ls -1
-    emacs -nw 0_pong.c
+    emacs -nw $PONG_SRC
 else
     echo "Executable \"\"\"./a.out\"\"\" does NOT exist."
 fi

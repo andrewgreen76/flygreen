a47f8834462d6d5764038558f1415e1729e162d9
Andrew Green <andrewgreen031991@gmail.com>
2025-02-25 22:52:05 -0800

    x

--- a_win-games+/l_c-mchlang/x_games/3_snake.sh
+++ a_win-games+/l_c-mchlang/x_games/3_snake.sh
@@ -1,9 +1,11 @@
-gcc 0_snake_dumb_crude.c -lm
+rm a.out
+gcc 4_snake_dumb_crude.c -lm
 
 if [ -e ./a.out ]; then
     read -n 1 -s -r -p "Press any key to continue..."
     ./a.out
     ls -1
+    emacs -nw 4_snake_dumb_crude.c
 else
     echo "Executable \"\"\"./a.out\"\"\" does NOT exist."
 fi

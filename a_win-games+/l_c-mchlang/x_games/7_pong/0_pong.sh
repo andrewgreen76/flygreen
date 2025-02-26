1cb1c9e0000c07721fd1d5525adeeb0d6499a57a
Andrew Green <andrewgreen031991@gmail.com>
2025-02-25 23:44:36 -0800

    x

--- a_win-games+/l_c-mchlang/x_games/7_pong/0_pong.sh
+++ a_win-games+/l_c-mchlang/x_games/7_pong/0_pong.sh
@@ -1,13 +1,17 @@
-PONG_SRC="pong.c"
+#!/bin/bash
 
-rm a.out
+BIN_EXE="./a.out"
+PONG_SRC="./src/pong.c"
+
 gcc $PONG_SRC -lm
 
-if [ -e ./a.out ]; then
+if [ -e $BIN_EXE ]; then
     read -n 1 -s -r -p "Press any key to continue..."
-    ./a.out
-    ls -1
+    $BIN_EXE
     emacs -nw $PONG_SRC
+    rm $BIN_EXE
+    clear
+    ls -1
 else
-    echo "Executable \"\"\"./a.out\"\"\" does NOT exist."
+    echo "Executable \"\"\"./" + $BIN_EXE + "\"\"\" does NOT exist."
 fi

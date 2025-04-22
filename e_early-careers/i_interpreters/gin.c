aa653cc381db6cf5c663ec9009a6479c8a25d2d2
Andrew Green <andrewgreen031991@gmail.com>
2025-04-22 00:12:26 -0700

    x

--- e_early-careers/i_interpreters/gin.c
+++ e_early-careers/i_interpreters/gin.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "ytypes.h"
+#include "gintypes.h"
 
 ///////////////////////////////////////////////////////////////
 
@@ -7,7 +7,7 @@ void main(){
   string uprompt;
   
   while(1){
-    printf("yokai : ");
+    printf("gin : ");
     if ( scanf("%s" , uprompt) == -1) { printf("\n"); break; }
     printf("You wrote: %s\n" , uprompt);
   }

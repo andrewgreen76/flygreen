b501d640446512ed1d0a152bfe43421ec232c918
Andrew Green <andrewgreen031991@gmail.com>
2025-04-26 18:54:03 -0700

    x

---
+++ e_early-careers/o_langs_parads_utils/7_cobol/0_run.sh
@@ -0,0 +1,1 @@
+cobc -x progname.cbl
---
+++ e_early-careers/o_langs_parads_utils/7_cobol/progname.cbl
@@ -0,0 +1,5 @@
+	IDENTIFICATION DIVISION.
+	PROGRAM-ID. PROGNAME.
+	PROCEDURE DIVISION.
+    	        DISPLAY "What the hell is this? Divisions?".
+    		STOP RUN.

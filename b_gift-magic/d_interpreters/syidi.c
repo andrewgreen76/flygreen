ac332faf4ec54b030452ec6c41e9935be1371f6d
Andrew Green <andrewgreen031991@gmail.com>
2025-04-29 08:59:07 -0700

    x

--- b_gift-magic/d_interpreters/syidi.c
+++ b_gift-magic/d_interpreters/syidi.c
@@ -7,7 +7,7 @@ void main(){
   string uprompt;
   
   while(1){
-    printf("syidi < ");
+    printf("SYIDI < ");
     if ( scanf("%s" , uprompt) == -1) { printf("\n"); break; }
     printf("You wrote: %s\n" , uprompt);
   }

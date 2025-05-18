d38c3e3af293591d13ba0531f6a4d7aded641a1c
Andrew Green <andrewgreen031991@gmail.com>
2025-05-18 01:39:16 -0700

    x

---
+++ a_win-games+/l_c-mchlang/t_pixelator/v0-1-5/include/ansi.h
@@ -0,0 +1,33 @@
+#ifndef ANSI_H
+#define ANSI_H
+
+#define HI_BLK  30
+#define HI_RED  31
+#define HI_GRN  32
+#define HI_GLD  33
+#define HI_BLU  34
+#define HI_PUR  35
+#define HI_TEA  36
+#define HI_WHT  37
+
+#define LO_BLK  40
+#define LO_RED  41
+#define LO_GRN  42
+#define LO_GLD  43
+#define LO_BLU  44
+#define LO_PUR  45
+#define LO_TEA  46
+#define LO_WHT  47
+
+#include <stdio.h>
+#include <stdint.h>
+#include "debug.h"
+#include "res.h"
+#include "timer.h"
+
+void clear_term();
+void reset_colors();
+void test_palette();
+void test_res();
+
+#endif
---
+++ a_win-games+/l_c-mchlang/t_pixelator/v0-1-5/src/ansi.c
@@ -0,0 +1,77 @@
+/* All the functions using ANSI escape codes,
+   including test functions. */
+
+#include "ansi.h"
+
+////////////////////////////////////////////////////////////////
+void test_res(){  
+  uint8_t clref = 30;
+  
+  printf("Performing a color test on the terminal (for finest resolution) ...\n");
+  delay(DLY_TICKS);
+  clear_term();
+
+  for( int r=0 ; r<RES_HEIGHT/2 ; r++){
+    for( int c=0 ; c<RES_WIDTH/2 ; c++ ){
+      printf( "\033[%d;%dm\u2580" , HI_WHT , LO_GRN );
+      printf( "\033[%d;%dm\u2580" , HI_GRN , LO_WHT );      
+    }
+    printf("\n");
+  }
+
+  printf("\033[0m\033[H");
+
+  for( int r=0 ; r<RES_HEIGHT/2 ; r++){
+    for( int c=0 ; c<RES_WIDTH/2 ; c++ ){
+      printf( "\033[%d;%dm\u2580" , HI_GLD , LO_WHT );
+      printf( "\033[%d;%dm\u2580" , HI_WHT , LO_GLD );      
+    }
+    printf("\n");
+  }
+
+  printf("\033[0m\033[H");
+
+  for( int r=0 ; r<RES_HEIGHT/2 ; r++){
+    for( int c=0 ; c<RES_WIDTH/2 ; c++ ){
+      printf( "\033[%d;%dm\u2580" , HI_WHT , LO_GRN );
+      printf( "\033[%d;%dm\u2580" , HI_GRN , LO_WHT );      
+    }
+    printf("\n");
+  }
+
+  printf("\033[0m\033[H");
+
+  for( int r=0 ; r<RES_HEIGHT/2 ; r++){
+    for( int c=0 ; c<RES_WIDTH/2 ; c++ ){
+      printf( "\033[%d;%dm\u2580" , HI_GLD , LO_WHT );
+      printf( "\033[%d;%dm\u2580" , HI_WHT , LO_GLD );      
+    }
+    printf("\n");
+  }
+  
+}
+
+////////////////////////////////////////////////////////////////
+void test_palette(){  
+  uint8_t ccode = 30; 
+  printf("Performing a colored characters display test ...\n");
+  
+  // fg: 29-37 ; bg: 39-47 ???
+  for( ; ccode<38 ; ccode++ )
+    printf("\033[%d;%dm\u2580" , ccode , ccode+10 );
+  reset_colors();
+  printf("[EOTEST]\n");
+  //fflush(stdout);  // Enforce immediate print of stdout to terminal. 
+}
+
+////////////////////////////////////////////////////////////////
+void reset_colors(){
+  printf("\033[0m");  //printf("Restored character colors to default.\n");
+}
+
+void clear_term(){
+  reset_colors();
+  printf("\033[2J");  //printf("Cleared the terminal with attributes in a full wraparound.\n");
+  printf("\033[H");   //printf("Cleared the terminal with attributes, wrapping to origin.\n");
+  // Or_simply_thi$: system("tput reset"); 
+}

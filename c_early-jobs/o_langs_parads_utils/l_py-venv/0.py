ee360ffb6b9a60e54684488051853c8b96ba4b48
Andrew Green <andrewgreen031991@gmail.com>
2025-09-28 05:00:52 -0700

    x

--- c_early-jobs/o_langs_parads_utils/l_py-venv/0.py
+++ c_early-jobs/o_langs_parads_utils/l_py-venv/0.py
@@ -1,15 +1,11 @@
-inqcnt = 0
-u = 3
-l = 1
-# => guess space : [1-3] 
-a = 2
+num = [5,2,0,-1]
+target = 4
 
-def half( u , l )
-  inqcnt += 1
-  g==a	# check the special case
-    ? print inqcnt
-    : a <= g=(u-l)/2  
-      ? half( u , g ) 
-      : half( g , l )
+log1 = 0
+log2 = 0
 
-half()
+
+
+# Now what do you want to do ?
+
+
---
+++ c_early-jobs/o_langs_parads_utils/l_py-venv/1_workouts/3_sizeofin.py
@@ -0,0 +1,5 @@
+#$ python3 [0.py] 3 -42 0.1
+
+import sys
+
+print( len(sys.argv) )

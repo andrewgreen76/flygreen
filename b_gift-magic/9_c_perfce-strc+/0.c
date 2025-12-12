92987361faaf6aa7e72aa814e64adf6e1c302fbb
Andrew Green <andrewgreen031991@gmail.com>
2025-12-11 21:01:58 -0800

    x

--- b_gift-magic/9_c_perfce-strc+/0.c
+++ b_gift-magic/9_c_perfce-strc+/0.c
@@ -1,28 +1,2 @@
-
-/////////////////////////////////////
-
-#include <stdio.h>
-
-#define SIZE 3
-int arr[SIZE] , t=0 ;
-
-/////////////////////////////////////
-
-void print_arr() {  
-  for( t=0 ; t<SIZE ; t++ ) {
-    arr[t]=t;
-    printf("%d : %d\n" , t , arr[t]);
-  }
-}
-
-/////////////////////////////////////
-
 void main(){
-  
-  print_arr();
-  
-  int d = 1;
-  arr[d] = d++;
-  
-  print_arr();  
 }

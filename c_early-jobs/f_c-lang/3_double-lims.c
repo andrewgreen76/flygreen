917b472f111cc10186a3b4b3cf17c52168d84388
Andrew Green <andrewgreen031991@gmail.com>
2026-01-11 08:33:17 -0800

    x

--- c_early-jobs/f_c-lang/3_double-lims.c
+++ c_early-jobs/f_c-lang/3_double-lims.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char * argv[]){
-  double d = 10.12345123451234512345;
-
-  printf("\n");
-  
-  printf("%.19f", d);
-
-  printf("\n");
+  double d = 10.12345123451234512345;  
+  printf("%.19f\n", d);
+  d = 101.2345123451234512345;    
+  printf("%.19f\n", d);
+  d = 10123451234512345.12345;    
+  printf("%.19f\n", d);
 }

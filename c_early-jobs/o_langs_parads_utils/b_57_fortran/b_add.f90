c46189b2db5916e42d2392dc052942ff09c617e4
Andrew Green <andrewgreen031991@gmail.com>
2026-01-11 01:49:59 -0800

    x

--- c_early-jobs/o_langs_parads_utils/b_57_fortran/b_add.f90
+++ c_early-jobs/o_langs_parads_utils/b_57_fortran/b_add.f90
@@ -4,16 +4,5 @@ program add_numbers
     a = 2
     b = 3
     sum = a + b
-
-    print *, 'The sum of', a, 'and', b, 'is', sum
-    print '(A,I0)', 'The sum of 2 and 3 is ', sum
-    print '(A,I0,A,I0,A,I0)', 'The sum of ', a, ' and ', b, ' is ', sum
-
-    ! Q: "Why are there tabs preceding the numerical arguments in the text output?"
-    ! A: "The tabs appear because Fortran’s `print *` statement inserts default spacing before each value to align
-    !     output in columns."
-    !
-    ! "In Fortran formats, `'A'` stands for a character string and `'I0'` stands for an integer with minimal width,
-    ! printing without leading spaces."
     
 end program add_numbers

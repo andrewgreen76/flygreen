779f4664fe2f8abefc2d40a8d14c418bf6ca53c4
Andrew Green <andrewgreen031991@gmail.com>
2026-03-29 19:16:02 -0700

    x

---
+++ c_early-jobs/f_c-lang/l_memory/g_padding/1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+struct A {
+  bool a;
+  double b;
+  int c;
+};
+
+struct B {
+  double a;
+  int b;
+  bool c;
+};
+
+void main(){
+  printf("Size of bool: %ld\n" , sizeof(bool) );
+  printf("Size of int: %ld\n" , sizeof(int) );
+  printf("Size of double: %ld\n" , sizeof(double) );
+
+  printf("Size of struct A: %ld\n" , sizeof(struct A) );
+  printf("Size of struct B: %ld\n" , sizeof(struct B) );  
+}
